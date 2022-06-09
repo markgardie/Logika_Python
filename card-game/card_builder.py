2051f883e30f3f766de2728d203f0af765a3d4e3
markgardie <markgardie@users.noreply.github.com>
2022-06-09 16:55:51 +0300

    add enemy card builder

--- card-game/card_builder.py
+++ card-game/card_builder.py
@@ -25,6 +25,22 @@ class CardBuilder():
     def get_list(self):
         return self.card_list
 
-        
 
+class EnemyCardBuilder():
+
+    def __init__(self):
+
+        
+        self.enemy = Card(
+                    width=CARD_WIDTH + 40, 
+                    height=CARD_HEIGHT + 30, 
+                    x = WINDOW_WIDTH//2 - CARD_WIDTH//2,
+                    y = WINDOW_HEIGHT//2 - CARD_HEIGHT,
+                    img_name="6.png")
+
+            
+        self.enemy.load_image()
+        
+    def get_enemy(self):
+        return self.enemy
             
