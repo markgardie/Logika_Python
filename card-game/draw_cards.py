0a751520b973264cfc73fb33ce5955e4e213a876
markgardie <markgardie@users.noreply.github.com>
2022-06-09 18:29:13 +0300

    return flag

--- card-game/draw_cards.py
+++ card-game/draw_cards.py
@@ -11,6 +11,8 @@ def draw_big_cards(flag, cards, window, clicked_card_num):
             if card.num != clicked_card_num:
                 window.blit(card.image, (card.x, card.y))
 
+    return flag
+
 
 def draw_small_cards(flag, cards, window, clicked_card_num):
 
