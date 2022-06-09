6fc87b8ecedad4a49305e44b510841e215caf9fa
markgardie <markgardie@users.noreply.github.com>
2022-06-09 16:59:26 +0300

    create draw functions

---
+++ card-game/draw_cards.py
@@ -0,0 +1,24 @@
+
+
+def draw_big_cards(flag, cards, window, clicked_card_num):
+    
+    if flag == 0:
+        for card in cards:
+            window.blit(card.image, (card.x, card.y))
+    else:
+        flag -= 1
+        for card in cards:
+            if card.num != clicked_card_num:
+                window.blit(card.image, (card.x, card.y))
+
+
+def draw_small_cards(flag, cards, window, clicked_card_num):
+
+    if flag > 0:
+        for card in cards:
+            if card.num == clicked_card_num:
+                window.blit(card.image, (card.x, card.y))
+
+
+def draw_enemy_card(enemy, window):
+    window.blit(enemy.image, (enemy.x, enemy.y))
