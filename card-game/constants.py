078b0f029fe869526112bb965cb0829741525085
markgardie <markgardie@users.noreply.github.com>
2022-06-09 16:56:43 +0300

    add new constants

--- card-game/constants.py
+++ card-game/constants.py
@@ -9,3 +9,14 @@ CARD_HEIGHT = 320
 LEMON = (213, 216, 165)
 BASE_PATH = os.path.abspath(__file__ + "/..")
 IMAGES_PATH = os.path.join(BASE_PATH, "images")
+ICON_PATH = os.path.join(IMAGES_PATH, "icon.jpg")
+FONT_PATH = os.path.join(BASE_PATH, "font/dialog.otf")
+FONT_SIZE = 60
+BLACK =  (255,255,255)
+SIZE = 10
+DICT_WARRIOR = {
+                    "HP": 9,
+                    "Attack": 3,
+                    "Defense": 5,
+                    "Crystal": 5
+                }
