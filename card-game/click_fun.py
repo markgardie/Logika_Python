bf80731fa40157bb124fa6c6a0c05a64d62be7b8
markgardie <markgardie@users.noreply.github.com>
2022-06-11 15:16:42 +0300

    add click function

---
+++ card-game/click_fun.py
@@ -0,0 +1,8 @@
+
+def on_card_click(cards, event):
+    x, y = event.pos
+
+    for card in cards:
+        if card.rect.collidepoint(x,y):
+            print(f"Карточка номер {card.card_num} була натиснута")
+            return card.card_num
