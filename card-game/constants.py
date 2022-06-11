09fd2b11d43341d6c7f6493522d8aabc9e252c6d
markgardie <markgardie@users.noreply.github.com>
2022-06-11 14:54:04 +0300

    change path

--- card-game/constants.py
+++ card-game/constants.py
@@ -8,4 +8,4 @@ CARD_WIDTH = 200
 CARD_HEIGHT = 320
 LEMON = (213, 216, 165)
 BASE_PATH = os.path.abspath(__file__ + "/..")
-IMAGES_PATH = os.path.join(BASE_PATH, "\images")
+IMAGES_PATH = os.path.join(BASE_PATH, "images")
