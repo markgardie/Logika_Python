4549ab442cac4df0b1a10c15e9ba644d1fd0197b
markgardie <markgardie@users.noreply.github.com>
2022-06-13 17:16:56 +0300

    add create_block function

---
+++ arkanoid/functions.py
@@ -0,0 +1,17 @@
+from constants import*
+from sprite import*
+
+def create_blocks(blocks):
+    x = 20 
+    y = 20 
+
+    for raw in MAP:
+        for place in raw:
+            if place == "1":
+                block = Sprite(BLOCK_WIDTH, BLOCK_HEIGHT, x, y, "block.png")
+                block.load_image()
+                blocks.append(block)
+            x += 76
+        y += 56
+        x = 20
+
