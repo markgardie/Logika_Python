294cf88cf6d5345e20908253829fe1dd652db5dd
markgardie <markgardie@users.noreply.github.com>
2022-06-13 16:48:33 +0300

    change icon path

--- arkanoid/constants.py
+++ arkanoid/constants.py
@@ -7,4 +7,5 @@ WINDOW_HEIGHT = 600
 BLUE = (200, 255, 255)
 PROJECT_PATH = os.path.abspath(__file__ + "/..")
 IMAGES_PATH = os.path.join(PROJECT_PATH, "images")
-ICON_PATH = os.path.join(IMAGES_PATH, "icon.jpg")
+ICON_PATH = os.path.join(IMAGES_PATH, "icon.png")
+FONT_PATH = os.path.join(PROJECT_PATH, "fonts")
