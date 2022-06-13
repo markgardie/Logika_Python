622156a3258011233f282231b5430abd3b0b64f1
markgardie <markgardie@users.noreply.github.com>
2022-06-13 16:58:52 +0300

    create sprite class

---
+++ arkanoid/sprite.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import*
+
+class Sprite():
+
+    def __init__(self, width = 10, height = 10, x = 0, y = 0, img_name = ""):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.img_name = img_name
+        self.img_path = os.path.join(IMAGES_PATH, img_name)
+        self.image = None
+        self.hitbox = pygame.Rect(x, y, width, height)
+
+    def load_image(self):
+        image = pygame.image.load(self.img_path)
+        self.image = pygame.transform.scale(image, (self.hitbox.width, self.hitbox.height))
