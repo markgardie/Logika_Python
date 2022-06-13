5d43fe21f3561717f7244492fb15ae631c6c33d7
markgardie <markgardie@users.noreply.github.com>
2022-06-13 16:17:36 +0300

    arkanoid project template

---
+++ arkanoid/constants.py
@@ -0,0 +1,10 @@
+
+import os
+
+
+WINDOW_WIDTH = 880
+WINDOW_HEIGHT = 600
+LEMON = (213, 216, 165)
+PROJECT_PATH = os.path.abspath(__file__ + "/..")
+IMAGES_PATH = os.path.join(PROJECT_PATH, "images")
+ICON_PATH = os.path.join(IMAGES_PATH, "icon.jpg")
---
+++ arkanoid/main.py
@@ -0,0 +1,26 @@
+from constants import*
+import pygame
+
+pygame.init()
+
+#------Window
+
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Card Game")
+
+
+fps = pygame.time.Clock()
+
+#------Game Cycle
+game = True
+
+
+while game:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+            
+    pygame.display.flip()
+    window.fill(LEMON)
+    fps.tick(60)
