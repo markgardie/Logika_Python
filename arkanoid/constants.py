74b88905cc99b92b1988b879e311201af4a64f4a
markgardie <markgardie@users.noreply.github.com>
2022-06-18 14:20:58 +0300

    add constants

---
+++ arkanoid/constants.py
@@ -0,0 +1,21 @@
+
+import os
+
+
+WINDOW_WIDTH = 880
+WINDOW_HEIGHT = 600
+BLOCK_WIDTH = 70
+BLOCK_HEIGHT = 30
+PLATFORM_WIDTH = 150
+PLATFORM_HEIGHT = 30
+BALL_WIDTH = 30
+BALL_HEIGHT = 30
+BLUE = (200, 255, 255)
+PROJECT_PATH = os.path.abspath(__file__ + "/..")
+IMAGES_PATH = os.path.join(PROJECT_PATH, "images")
+ICON_PATH = os.path.join(IMAGES_PATH, "icon.png")
+FONT_PATH = os.path.join(PROJECT_PATH, "fonts")
+MAP = ["1111111111",
+        "0001111000",
+        "1111111111",
+        "0001111000"]
