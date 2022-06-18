a9408a67f7c9b1c5f94f387d5314c91ae55d7693
markgardie <markgardie@users.noreply.github.com>
2022-06-18 14:37:05 +0300

    create main

---
+++ arkanoid/main.py
@@ -0,0 +1,26 @@
+from constants import*
+import pygame
+
+pygame.init()
+
+#------Window
+
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Arkanoid")
+
+
+fps = pygame.time.Clock()
+
+#------Game Cycle
+game = True
+
+
+while game:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    pygame.display.flip()
+    window.fill(BLUE)
+    fps.tick(60)
