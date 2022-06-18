41f229040248dc682aff34ad64c8c4ee27eae892
markgardie <markgardie@users.noreply.github.com>
2022-06-18 17:35:27 +0300

    add functions

---
+++ arkanoid/functions.py
@@ -0,0 +1,22 @@
+from constants import*
+import pygame
+from sprite import*
+
+def create_blocks(blocks):
+    x = 20 
+    y = 20 
+
+    for row in MAP:
+        for place in row:
+            if place == "1":
+                block = Sprite(BLOCK_WIDTH, BLOCK_HEIGHT, x, y, "block.png")
+                block.load_image()
+                blocks.append(block)
+            x += 76
+        y += 56
+        x = 20
+
+def draw_blocks(blocks, window):
+    
+    for block in blocks:
+        window.blit(block.image, (block.x, block.y))
