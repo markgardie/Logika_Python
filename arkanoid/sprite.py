a558e04d0e5f50ceaee7c3f83e38a6c962ddadbf
markgardie <markgardie@users.noreply.github.com>
2022-06-23 15:19:59 +0300

    add handle keys method

--- arkanoid/sprite.py
+++ arkanoid/sprite.py
@@ -19,6 +19,13 @@ class Sprite():
         image = pygame.image.load(self.img_path)
         self.image = pygame.transform.scale(image, (self.hitbox.width, self.hitbox.height))
 
+    def handle_keys(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.move_left()
+        if keys[pygame.K_d]:
+            self.move_right()
+    
     def move_left(self):
         self.x -= self.speed
         self.hitbox.x -= self.speed
