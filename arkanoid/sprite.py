bfde68cafd9939a7796ca3c659b2caafd10b2686
markgardie <markgardie@users.noreply.github.com>
2022-06-25 15:09:57 +0300

    add load_image method

--- arkanoid/sprite.py
+++ arkanoid/sprite.py
@@ -1,15 +1,18 @@
-from constants import*
 import pygame
+from constants import*
 
 class Sprite():
 
-    def __init__(self, width, height, x, y, img_name):
+    def __init__(self, width = 10, height = 10, x = 0, y = 0, img_name = ""):
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         self.img_name = img_name
-
-        self.image = None
         self.img_path = os.path.join(IMAGES_PATH, img_name)
+        self.image = None
         self.hitbox = pygame.Rect(x, y, width, height)
+
+    def load_image(self):
+        image = pygame.image.load(self.img_path)
+        self.image = pygame.transform.scale(image, (self.hitbox.width, self.hitbox.height))
