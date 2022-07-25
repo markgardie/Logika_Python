fa9df7540c6dff7c49fbeba6c6cda9e0b45ca532
markgardie <markgardie@users.noreply.github.com>
2022-07-25 17:59:37 +0300

    copy code to main fun

--- slide_puzzle/kovalevsky.py
+++ slide_puzzle/kovalevsky.py
@@ -1,30 +1,31 @@
 from constants import*
 import pygame
 
-pygame.init()
+def main():
+    pygame.init()
 
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
-pygame.display.set_caption("Slide Puzzle")
-
-
-fps = pygame.time.Clock()
-pygame.display.flip()
-window.fill(BLACK)
-fps.tick(60)
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
+    pygame.display.set_caption("Slide Puzzle")
 
 
-game = True
-
-while game:
-
-    for event in pygame.event.get():
-        
-        if event.type == pygame.QUIT:
-            game = False
-
+    fps = pygame.time.Clock()
     pygame.display.flip()
     window.fill(BLACK)
     fps.tick(60)
+
+
+    game = True
+
+    while game:
+
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                game = False
+
+        pygame.display.flip()
+        window.fill(BLACK)
+        fps.tick(60)
 
 
 def getRandomMove(board,lastMove = None):
