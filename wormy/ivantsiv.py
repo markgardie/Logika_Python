110650b80114235335d6610d2bef5714fb9abd6f
markgardie <markgardie@users.noreply.github.com>
2022-07-28 19:50:54 +0300

    add max's functions

---
+++ wormy/ivantsiv.py
@@ -0,0 +1,50 @@
+import pygame
+import sys
+pygame.init()
+
+
+
+def terminate():
+    pygame.quit() 
+    sys.exit()
+
+def getRandomLocation():
+    return {'x': random.randint(0, CELLWIDTH - 1), 'y': random.randint(0, CELLHEIGHT - 1)}
+
+def drawGrid():
+
+    for x in range(0, WINDOW_WIDTH, CELL_SIZE):
+        pygame.draw.line(window, DARK_GRAY, (x, 0), (x, WINDOW_HEIGHT))
+    for y in range(0, WINDOW_HEIGHT, CELL_SIZE):
+        pygame.draw.line(window, DARK_GRAY, (0, y), (WINDOW_WIDTH, 0))
+
+
+def handleLose(wormCoords):
+    
+    if wormCoords[HEAD]['x'] == -1 or wormCoords[HEAD]['x'] == CELLWIDTH or wormCoords[HEAD]['y'] == -1 or wormCoords[HEAD]['y'] == CELLHEIGHT:
+        return # game over
+
+    for wormBody in wormCoords[1:]:
+        if wormBody['x'] == wormCoords[HEAD]['x'] and wormBody['y'] == wormCoords[HEAD]['y']:
+            return # game over
+
+def handleAppleTouch(wormCoords):
+    # check if worm has eaten an apply
+    if wormCoords[HEAD]['x'] == apple['x'] and wormCoords[HEAD]['y'] == apple['y']:
+            # don't remove worm's tail segment
+        apple = getRandomLocation() # set a new apple somewhere
+    else:
+        del wormCoords[-1] # remove worm's tail segment
+
+def newCoords(wormCoords, direction):
+     # move the worm by adding a segment in the direction it is moving
+    if direction == UP:
+        newHead = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] - 1}
+    elif direction == DOWN:
+        newHead = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] + 1}
+    elif direction == LEFT:
+        newHead = {'x': wormCoords[HEAD]['x'] - 1, 'y': wormCoords[HEAD]['y']}
+    elif direction == RIGHT:
+        newHead = {'x': wormCoords[HEAD]['x'] + 1, 'y': wormCoords[HEAD]['y']}
+    
+    wormCoords.insert(0, newHead)
