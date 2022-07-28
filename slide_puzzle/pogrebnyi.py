0bc539a7c980c9e1100b515b6f01b9a081df1077
markgardie <markgardie@users.noreply.github.com>
2022-07-28 19:59:58 +0300

    add getLeftTopOfTile, drawBoard functions

--- slide_puzzle/pogrebnyi.py
+++ slide_puzzle/pogrebnyi.py
@@ -6,4 +6,21 @@ pygame.init()
 def terminate():
     pygame.quit()
     sys.exit()
-   
+
+
+def getLeftTopOfTile(tileX, tileY):
+    left = XMARGIN + (tileX * TILESIZE) + (tileX - 1)
+    top = YMARGIN + (tileY * TILESIZE) + (tileY - 1)
+    return (left, top)
+
+
+def drawBoard(board, message):
+    window.fill(BGCOLOR)
+    if message:
+        textSurf, textRect = makeText(message, MESSAGECOLOR, BGCOLOR, 5, 5)
+        window.blit(textSurf, textRect)
+
+    for tilex in range(len(board)):
+        for tiley in range(len(board[0])):
+            if board[tilex][tiley]:
+                drawTile(tilex, tiley, board[tilex][tiley])
