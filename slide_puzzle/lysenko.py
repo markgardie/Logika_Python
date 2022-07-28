36c00211f1bec275ccd7506aabbbd4d35b0c8566
markgardie <markgardie@users.noreply.github.com>
2022-07-28 20:51:04 +0300

    def  handleClicks(): delete

--- slide_puzzle/lysenko.py
+++ slide_puzzle/lysenko.py
@@ -50,32 +50,3 @@ def initialState ():
     SOLVEDBOARD = getStartingBoard() # a solved board is the same as the board in a start state. 
     allMoves = [] # list of moves made from the solved configuration 
  
- 
-def  handleClicks(): 
-    for event in pygame.event.get(): # event handling loop 
-            if event.type == MOUSEBUTTONUP: 
-                spotx, spoty = getSpotClicked(mainBoard, event.pos[0], event.pos[1]) 
- 
-                if (spotx, spoty) == (None, None): 
-                    # check if the user clicked on an option button 
-                    if RESET_RECT.collidepoint(event.pos): 
-                        resetAnimation(mainBoard, allMoves) # clicked on Reset button 
-                        allMoves = [] 
-                    elif NEW_RECT.collidepoint(event.pos): 
-                        mainBoard, solutionSeq = generateNewPuzzle(80) # clicked on New Game button 
-                        allMoves = [] 
-                    elif SOLVE_RECT.collidepoint(event.pos): 
-                        resetAnimation(mainBoard, solutionSeq + allMoves) # clicked on Solve button 
-                        allMoves = [] 
-                else: 
-                    # check if the clicked tile was next to the blank spot 
- 
-                    blankx, blanky = getBlankPosition(mainBoard) 
-                    if spotx == blankx + 1 and spoty == blanky: 
-                        slideTo = LEFT 
-                    elif spotx == blankx - 1 and spoty == blanky: 
-                        slideTo = RIGHT 
-                    elif spotx == blankx and spoty == blanky + 1: 
-                        slideTo = UP 
-                    elif spotx == blankx and spoty == blanky - 1: 
-                        slideTo = DOWN
