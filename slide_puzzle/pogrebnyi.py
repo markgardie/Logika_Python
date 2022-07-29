06e8888fc396851e3f67108304e255e14707c983
markgardie <markgardie@users.noreply.github.com>
2022-07-29 18:13:01 +0300

    add resetAnimation fun

--- slide_puzzle/pogrebnyi.py
+++ slide_puzzle/pogrebnyi.py
@@ -16,6 +16,7 @@ def getLeftTopOfTile(tileX, tileY):
 
 def drawBoard(board, message):
     window.fill(BGCOLOR)
+    
     if message:
         textSurf, textRect = makeText(message, MESSAGECOLOR, BGCOLOR, 5, 5)
         window.blit(textSurf, textRect)
@@ -24,3 +25,21 @@ def drawBoard(board, message):
         for tiley in range(len(board[0])):
             if board[tilex][tiley]:
                 drawTile(tilex, tiley, board[tilex][tiley])
+
+
+def resetAnimation(board, allMoves):
+    # make all of the moves in allMoves in reverse.
+    revAllMoves = allMoves[:] # gets a copy of the list
+    revAllMoves.reverse()
+
+    for move in revAllMoves: 
+        if move == UP:
+            oppositeMove = DOWN
+        elif move == DOWN:
+            oppositeMove = UP
+        elif move == RIGHT:
+            oppositeMove = LEFT
+        elif move == LEFT:
+            oppositeMove = RIGHT
+        slideAnimation(board, oppositeMove, '', animationSpeed=int(TILESIZE / 2))
+        makeMove(board, oppositeMove)
