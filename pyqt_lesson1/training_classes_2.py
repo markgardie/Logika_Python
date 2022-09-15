02d199e1e9c89a9a3366e7720cc3f090d954d88b
markgardie <markgardie@users.noreply.github.com>
2022-09-15 16:46:03 +0300

    vsc_classes_ex2_solution

--- pyqt_lesson1/training_classes_2.py
+++ pyqt_lesson1/training_classes_2.py
@@ -1,7 +1,22 @@
 class Title():
-    #конструктор
-    
-    #методи
-
-#створи два написи
-#приховай другий напис
+    def __init__(self, title_text, x_num, y_num):
+        self.title = title_text
+        self.x = x_num
+        self.y = y_num
+        self.appearance = True
+    def hide(self):
+        self.appearance = False
+        print(self.title, '- приховано')
+    def show(self):
+        self.appearance = True
+        print(self.title, '- відображається')
+    def print_info(self):
+        print('Кнопка:', self.title)
+        print('Розташування:', '(' + str(self.x) + ',' + str(self.y) + ')')
+        print('Видимість:', self.appearance)
+ 
+main_title = Title('Дізнатися переможця прямо зараз!', 150, 50)
+rules_title = Title('Переможець може бути тільки один', 150, -100)
+main_title.print_info()
+rules_title.print_info()
+rules_title.hide()
