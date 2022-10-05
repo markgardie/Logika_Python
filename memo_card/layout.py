b58859c8cee223a8237e66ee196a268ec2208850
markgardie <markgardie@users.noreply.github.com>
2022-10-05 20:07:45 +0300

    creat card_layout widgets

--- memo_card/layout.py
+++ memo_card/layout.py
@@ -3,5 +3,27 @@ from PyQt5.QtWidgets import (
        QApplication, QHBoxLayout, QVBoxLayout,
        QGroupBox, QButtonGroup, QRadioButton, 
        QPushButton, QLabel, QSpinBox)
+from constants import*
 
-    
+
+menuButton = QPushButton("Меню")
+timerButton = QPushButton("Відпочити")
+answerButton = QPushButton("Відповісти")
+
+
+ansRadioButton1 = QRadioButton(RIGHT_TEXT)
+ansRadioButton2 = QRadioButton(WRONG_TEXT1)
+ansRadioButton3 = QRadioButton(WRONG_TEXT2)
+ansRadioButton4 = QRadioButton(WRONG_TEXT3)
+
+
+timer = QSpinBox()
+timer.setValue(30)
+
+questionLabel = QLabel(QUESTION_TEXT)
+minutesLabel = QLabel("хвилин")
+
+ansButtonGroup = QButtonGroup()
+ansGroupBox = QGroupBox("Варіанти відповідей")
+
+
