fa67f034ed2f9c9a866edc172b0cfd1e8f0caf99
markgardie <markgardie@users.noreply.github.com>
2022-10-19 18:20:26 +0300

    add list, functions calls

--- memory_card/main.py
+++ memory_card/main.py
@@ -1,24 +1,19 @@
 from app import app
-from layout.question_layout import (
-   mainColumn, app,
-   questionLabel, correctAnswerLabel, resultLabel,
-   ansButton1, ansButton2, ansButton3, ansButton4,
-   answerButton
-)
-from functions import showQuestion, showResult, showData, clickOK
-from PyQt5.QtWidgets import QWidget, QApplication
+from layouts.question_layout import questionWindow
+from functions import *
 from random import shuffle # будем перемешивать ответы в карточке вопроса
-from constants import*
 
 
+ansList = [ansRadioButton1, ansRadioButton2, ansRadioButton3, ansRadioButton4]
+shuffle(ansList)
+answer = ansList[0] # мы не знаем, какой это из радиобаттонов, но можем положить сюда правильный ответ и запомнить это
+wrongAnswer1, wrongAnswer2, wrongAnswer3 = ansList[1], ansList[2], ansList[3]
 
-#Створити список радіо-кнопок відповідей
-#Перемішати їх
-#Визначаємо перший елемент як правильну відповідь
-#Запускаємо функцію показу даних
-#Запускаємо функцію показу питання
-#Прописуємо підписку на подію кліку по кнопці "Відповісти"
-answerButton.clicked.connect(lambda: clickOK())
 
-#Показати вікно питання
-#Запустити додаток
+showData(answer, wrongAnswer1, wrongAnswer2, wrongAnswer3)
+showQuestion()
+
+answerButton.clicked.connect(lambda: clickOK(answer, wrongAnswer1, wrongAnswer2, wrongAnswer3))
+
+questionWindow.show()
+app.exec_()
