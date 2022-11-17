aac7ef22fc454e222774a3686d75f7f895e81074
markgardie <markgardie@users.noreply.github.com>
2022-11-17 11:43:51 +0200

    data functions

---
+++ notes/data/data.py
@@ -0,0 +1,19 @@
+
+import json
+from ui.mainWindow import uiMainWindow
+from PyQt5.QtWidgets import QInputDialog
+
+
+path = r"C:\Users\Марк\Desktop\Logika_Python\notes\data\notes.json"
+
+
+def readNotes():
+    with open(path, "r", encoding="utf-8") as file:
+        notes = json.load(file)
+    uiMainWindow.notesListWidget.addItems(notes)
+
+    return notes
+
+def writeNotes(newData):
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(newData, file)
