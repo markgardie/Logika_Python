df9c944645d4e2ed17f791eb983bfb298c79c1ec
markgardie <markgardie@users.noreply.github.com>
2022-11-30 16:00:48 +0200

    pil intro task 1 solution

--- pil_intro/main.py
+++ pil_intro/main.py
@@ -1,9 +1,36 @@
-from PIL import ...
+from PIL import Image, ImageFilter, ImageEnhance
 
-#открой файл с оригиналом картинки
+originalPath = r"C:\Users\Марк\Desktop\Logika_Python\pil_intro\original.jpg"
+savePath = r"C:\Users\Марк\Desktop\Logika_Python\pil_intro"
 
-#сделай оригинал изображения чёрно-белым
+with Image.open(originalPath) as original:
 
-#сделай оригинал изображения размытым
+    print('Розмір:', original.size)
+    print('Формат:', original.format)
+    print('Тип:', original.mode) 
+    original.show()
+ 
+    gray = original.convert('L')    
+    gray.save(savePath + r"\gray.jpg")
+    print('Розмір:', gray.size)
+    print('Формат:', gray.format)
+    print('Тип:', gray.mode) 
+    gray.show()
+ 
+    blured = original.filter(ImageFilter.BLUR)
+    blured.save(savePath + r'\blured.jpg')
+    blured.show()
+ 
+    up = original.transpose(Image.ROTATE_180)
+    up.save(savePath + r'\up.jpg')
+    up.show()
+ 
 
-#поверни оригинал изображения на 180 градусов
+    mirrow = original.transpose(Image.FLIP_LEFT_RIGHT)
+    mirrow.save(savePath + r'\mirrow.jpg')
+    mirrow.show()
+ 
+    contrast = ImageEnhance.Contrast(original)
+    contrast = contrast.enhance(1.5)
+    contrast.save(savePath + r'\contr.jpg')
+    contrast.show()
