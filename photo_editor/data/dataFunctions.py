34bc9acea5eba46dcde15ca494529f67371ae1c4
markgardie <markgardie@users.noreply.github.com>
2022-12-19 17:42:09 +0200

    add comments

--- photo_editor/data/dataFunctions.py
+++ photo_editor/data/dataFunctions.py
@@ -2,26 +2,29 @@ import os
 from PyQt5.QtWidgets import QFileDialog
 from ui.photoEditorUi import*
 
-extensions = ['.jpg','.jpeg', '.png', '.gif', '.bmp']
+extensions = ['.jpg','.jpeg', '.png', '.gif', '.bmp'] #розширення, які показують, що файл є растровим зображенням
 
+#Функція, яка запускає діалогове вікно, вибору папки
 def chooseWorkdir():
-    workdir = QFileDialog.getExistingDirectory()
-    return workdir
+    workdir = QFileDialog.getExistingDirectory() #Відкривається діалогове вікно, QFileDialog, 
+    #гравець обирає в цьому вікні папку з фото
+    #ми отримуємо шлях до цієї папки і зберігаємо в workdir
+    return workdir #повертаємо цей шлях, аби використовувати в інших функціях
 
 def filter(files, extensions):
-    filtered = []
-    for file in files:
-        for ext in extensions:
-            if file.endswith(ext):
-                filtered.append(file)
-    return filtered
+    filtered = [] #список відфільтрованих файлів (повинні бути фото), напочатку пустий
+    for file in files: #цикл, який перебирає файли в обраній папці
+        for ext in extensions: #цикл, який перевіряє розширення
+            if file.endswith(ext): #якщо файл закінчується розширенням фото, то
+                filtered.append(file) #додаємо цей файл в список відфільтрованих
+    return filtered #повертаємо список відфільтрованих файлів, аби використати в інших функціях
     
 def showFilenameList():
-    workdir = chooseWorkdir()
-    files = os.listdir(workdir)
-    filtered = filter(files, extensions)
+    workdir = chooseWorkdir() #запускаємо функцію для отримання папки з файлами
+    files = os.listdir(workdir) #отримуємо файл з цієї папки
+    filtered = filter(files, extensions) #фільтруємо папки
 
-    ui.photoListWidget.clear()
+    ui.photoListWidget.clear() #очищаємо список-віджет від попередніх файлів
 
-    for file in filtered:
-        ui.photoListWidget.addItem(file)
+    for file in filtered: #проходимось по відфільтрованим файлам
+        ui.photoListWidget.addItem(file) #додаємо назви цих файлів в віджет-список
--- photo_editor/logic/__pycache__/ImageProcessor.py
+++ photo_editor/logic/__pycache__/ImageProcessor.py
@@ -6,28 +6,27 @@ from PyQt5.QtGui import QPixmap
 class ImageProcessor():
 
     def __init__(self):
-        self.image = None
-        self.dir = None
-        self.filename = None
-        self.saveDir = "Modified"
+        self.image = None #зображення, поки ми його не відкрили, тому властивість пуска
+        self.dir = None #шлях до папки із зображеннями, не відкрили, пустка
+        self.filename = None #назва файлу-фото
+        self.saveDir = "Modified" #всі модифіковані файли будуть зберігатись в підпапку Modified
 
     def setDir(self, dir):
-        self.dir = dir
+        self.dir = dir #отримуємо за допомогою дата-функцій шлях до папки, надаємо цей шлях нашому обробнику фото (ImageProcessor)
 
     def loadImage(self, filename):
-        self.filename = filename
-        imagePath = os.path.join(self.dir, filename)
-        self.image = Image.load(imagePath)
+        self.filename = filename #зберігаємо назву файлу в нашому обробнику 
+        imagePath = os.path.join(self.dir, filename) #з'єднуємо шлях до папки і назву файлу, отримуємо повний шлях до файлу
+        self.image = Image.load(imagePath) #завантажуємо зображення по шлях вище
 
     def showImage(self):
-        ui.photoLabel.clear()
-        pixmapImage = QPixmap()
-        w = ui.photoLabel.width()
-        h = ui.photoLabel.height()
-        pixmapImage = pixmapImage.scaled(w, h)
-        ui.photoLabel.setPixmap(pixmapImage)
-        ui.photoLabel.show()
+        ui.photoLabel.clear() #очищаємо наш лейбл від попереднього фото
+        pixmapImage = QPixmap() #створюємо сітку (QPixmap), за допомогою якої накладемо фото поверх лейбла
+        w = ui.photoLabel.width() #отримуємо ширину лейбла
+        h = ui.photoLabel.height() #отримуємо висоту лейбла, це треба для того, аби фото було розмір із лейбл
+        pixmapImage = pixmapImage.scaled(w, h) #змінюємо розмір сітки, аби вона була розміром із лейбл
+        ui.photoLabel.setPixmap(pixmapImage) #накладаємо сітку на лейбл
+        ui.photoLabel.show() #показуємо лейбл
 
-    def showChosenImage(self):
-        if ui.photoListWidget.currentRow() > 0:
+    
             
