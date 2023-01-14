d4b486de23fd169f064a7a39c79e9894ffe092f7
markgardie <markgardie@users.noreply.github.com>
2023-01-14 16:13:34 +0200

    maze, 1st lesson

--- touch_and_go/main.py
+++ touch_and_go/main.py
@@ -5,3 +5,21 @@ from constants import*
 window = Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_CAPTION, BACKGROUND_PATH)
 sprite1 = Sprite(SPRITE_WIDTH, SPRITE_HEIGHT, SPRITE1_PATH, SPRITE1_X, SPRITE1_Y, SPEED)
 sprite2 = Sprite(SPRITE_WIDTH, SPRITE_HEIGHT, SPRITE2_PATH, SPRITE2_X, SPRITE2_Y, SPEED)
+
+game = True
+
+while game: 
+
+    window.display.blit(window.bg_image, (0, 0))
+    window.display.blit(sprite1.image, (sprite1.x, sprite1.y))
+    window.display.blit(sprite2.image, (sprite2.x, sprite2.y))
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            game = False
+        
+    
+    window.display.flip()
+    window.clock.tick(FPS)
+    
+
