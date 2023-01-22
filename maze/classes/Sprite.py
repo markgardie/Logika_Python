bfb10ed6ecadebf72473f9a152cbfb4e43b01d7e
markgardie <markgardie@users.noreply.github.com>
2023-01-22 13:05:56 +0200

    maze, 2nd lesson

---
+++ maze/classes/Sprite.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+
+class Sprite():
+
+    def __init__(self, width,  height, image_path, x, y, speed):
+        self.hitbox = pygame.Rect(x, y, width, height)
+
+        self.speed = speed
+
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (width, height))
+
+        #self.hitbox = self.image.get_rect()
+        #self.hitbox.x = x
+        #self.hitbox.y = y
+
+
+
+
---
+++ maze/main.py
@@ -0,0 +1,34 @@
+from constants import*
+from classes.Sprite import*
+from classes.Window import*
+# from classes.Player import*
+# from classes.Enemy import*
+
+window = Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_CAPTION, BACKGROUND_PATH)
+
+player = Sprite(SPRITE_WIDTH, SPRITE_HEIGHT, PLAYER_PATH, PLAYER_X, PLAYER_Y, PLAYER_SPEED)
+monster = Sprite(SPRITE_WIDTH, SPRITE_HEIGHT, MONSTER_PATH, MONSTER_X, MONSTER_Y, MONSTER_SPEED)
+goal = Sprite(SPRITE_WIDTH, SPRITE_HEIGHT, GOAL_PATH, GOAL_X, GOAL_Y, GOAL_SPEED)
+
+pygame.mixer.music.load(MUSIC_PATH)
+pygame.mixer.music.play()
+
+game = True
+
+while game:
+
+    window.display.blit(window.bg_image, (0,0))
+
+    window.display.blit(player.image, (player.hitbox.x, player.hitbox.y))
+    window.display.blit(monster.image, (monster.hitbox.x, monster.hitbox.y))
+    window.display.blit(goal.image, (goal.hitbox.x, goal.hitbox.y))
+
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            game = False
+
+    
+    pygame.display.flip()
+    window.clock.tick(FPS)
+    
