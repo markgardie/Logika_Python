90888b0353c4b29d668a141489443ecd9ba88d22
markgardie <markgardie@users.noreply.github.com>
2023-02-05 09:35:03 +0200

    arkanoid, 1st lesson

---
+++ arkanoid/Window.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+class Window():
+
+    def __init__(self, width, height, bg_color, caption):
+
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption(caption)
+        self.screen.fill(bg_color)
+
+        self.clock = pygame.time.Clock() 
