fbb6a27637667175e6d2fefdc32fd326f11e13c5
markgardie <markgardie@users.noreply.github.com>
2023-02-18 11:34:56 +0200

    arkanoid, lesson 3

---
+++ arkanoid/Platform.py
@@ -0,0 +1,10 @@
+from Sprite import*
+
+class Platform(Sprite):
+
+    def controls(self, left, right):
+
+        keys = pygame.key.get_pressed()
+
+        if keys[left] and self.hitbox.x > 5:
+            self.hitbox.x -= self.speed
--- arkanoid/Sprite.py
+++ arkanoid/Sprite.py
@@ -1,4 +1,6 @@
 import pygame
+import os
+from constants import*
 
 pygame.init()
 
@@ -6,6 +8,12 @@ class Sprite():
 
     def __init__(self, width = 10, height = 10, x = 0, y = 0, img_name = "", speed = 0):
         self.hitbox = pygame.Rect(x, y, width, height)
+
+        self.path = os.path.join(IMAGES_PATH, img_name)
+        self.image = pygame.image.load(self.path)
+        self.image = pygame.transform.scale(self.image, (width, height))
+
+        self.speed = speed
 
         
         
