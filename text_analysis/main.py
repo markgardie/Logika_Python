d441711caa18c5ef43c85c4695037e7fda3aa6d9
markgardie <markgardie@users.noreply.github.com>
2023-02-19 10:04:00 +0200

    text analysis, lesson 2

--- text_analysis/main.py
+++ text_analysis/main.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 
 def analyze_one_file():
 
-    path = r"C:\Users\Марк\Desktop\Logika_Python\text_analysis\good_movies\Shrek.txt"
+    path = r"C:\Users\Марк\Desktop\Logika_Python\text_analysis\Terminator Genisys.txt"
 
     text = ""
 
@@ -29,3 +29,20 @@ def analyze_one_file():
     for sentence in sentences:
         score = sentiment_analyzer.polarity_scores(sentence)["compound"]
         scores.append(score)
+
+    
+    scores_dct = dct(scores, norm = "ortho", type = 2)
+    scores_dct[10:] = 0
+    scores_filtered = idct(scores_dct, norm="ortho")
+
+    narrative_time = 100
+
+    scores_normalized = np.interp(np.linspace(0, 1, narrative_time), np.linspace(0, 1, len(scores_filtered)), scores_filtered)
+
+
+    plt.plot(scores_normalized)
+    plt.title("Terminator Genisys")
+    plt.savefig(r"C:\Users\Марк\Desktop\Logika_Python\text_analysis\Terminator Genisys")
+
+
+analyze_one_file()
