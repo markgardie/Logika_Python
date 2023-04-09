9c1f113a6e63a49059a7d1adefff23b48ce9aeed
markgardie <markgardie@users.noreply.github.com>
2023-04-09 11:36:12 +0300

    final, lesson 3

--- boss_fight/Boss.py
+++ boss_fight/Boss.py
@@ -9,7 +9,7 @@ class Boss(Sprite):
         direction_x = randint(-1, 1)
         direction_y = randint(-1, 1)
 
-        if direction_x == 0 and direction_y == 0:
+        while direction_x == 0 and direction_y == 0:
             direction_x = randint(-1, 1)
             direction_y = randint(-1, 1)
 
@@ -21,7 +21,59 @@ class Boss(Sprite):
         dir_2_x, dir_2_y = self.generate_direction()
         dir_3_x, dir_3_y = self.generate_direction()
 
-        #TODO
+        dir_xs = [dir_1_x, dir_2_x, dir_3_x]
+        dir_ys = [dir_1_y, dir_2_y, dir_3_y]
 
+        for dir_x in dir_xs:
+            for dir_y in dir_ys:
 
-        fireball1 = Fireball(FIREBALL_WIDTH, FIREBALL_HEIGHT, )
+                
+                # left-top
+                if dir_x == -1 and dir_y == -1:
+                    fireball = Fireball(FIREBALL_WIDTH, FIREBALL_HEIGHT,
+                                        self.hitbox.left, self.hitbox.top,
+                                        FIREBALL_PATH, FIREBALL_SPEED)
+                
+                # left
+                if dir_x == -1 and dir_y == 0:
+                    fireball = Fireball(FIREBALL_WIDTH, FIREBALL_HEIGHT,
+                                        self.hitbox.left, self.hitbox.centery,
+                                        FIREBALL_PATH, FIREBALL_SPEED)
+                    
+                # left-bottom
+                if dir_x == -1 and dir_y == 1:
+                    fireball = Fireball(FIREBALL_WIDTH, FIREBALL_HEIGHT,
+                                        self.hitbox.left, self.hitbox.bottom,
+                                        FIREBALL_PATH, FIREBALL_SPEED)
+                    
+                # top
+                if dir_x == 0 and dir_y == -1:
+                    fireball = Fireball(FIREBALL_WIDTH, FIREBALL_HEIGHT,
+                                        self.hitbox.centerx, self.hitbox.top,
+                                        FIREBALL_PATH, FIREBALL_SPEED)
+
+                # bottom
+                if dir_x == 0 and dir_y == 1:
+                    fireball = Fireball(FIREBALL_WIDTH, FIREBALL_HEIGHT,
+                                        self.hitbox.centerx, self.hitbox.bottom,
+                                        FIREBALL_PATH, FIREBALL_SPEED)
+                    
+                # right-top
+                if dir_x == 1 and dir_y == -1:
+                    fireball = Fireball(FIREBALL_WIDTH, FIREBALL_HEIGHT,
+                                        self.hitbox.right, self.hitbox.top,
+                                        FIREBALL_PATH, FIREBALL_SPEED)
+                    
+                # right
+                if dir_x == 1 and dir_y == 0:
+                    fireball = Fireball(FIREBALL_WIDTH, FIREBALL_HEIGHT,
+                                        self.hitbox.right, self.hitbox.centery,
+                                        FIREBALL_PATH, FIREBALL_SPEED)
+                
+                # right-bottom
+                if dir_x == 1 and dir_y == 1:
+                    fireball = Fireball(FIREBALL_WIDTH, FIREBALL_HEIGHT,
+                                        self.hitbox.right, self.hitbox.bottom,
+                                        FIREBALL_PATH, FIREBALL_SPEED)
+                    
+  
---
+++ boss_fight/main.py
@@ -0,0 +1,10 @@
+from constants import*
+from functions import*
+from Window import*
+from Boss import*
+from Player import*
+from Fireball import*
+
+window = Window(WINDOW_WIDTH, WINDOW_HEIGHT, CAPTION, BLUE)
+
+boss = Boss(BOSS_WIDTH, BOSS_HEIGHT, BOSS_X, BOSS_Y, BOSS_PATH, BOSS_SPEED)
