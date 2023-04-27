0f42a51d5560a6721e9095e0efebec6bac80cf74
markgardie <markgardie@users.noreply.github.com>
2023-04-27 17:54:43 +0300

    platformer 2, ver. 1.0

--- platformer2/functions.py
+++ platformer2/functions.py
@@ -1,13 +1,13 @@
 from constants import*
 import pygame
 
-def win_lose(coin, player, enemy):
+def win_lose(player, enemy, scores):
 
     font = pygame.font.Font(None, 50)
     finish = False
     text = ""
 
-    if player.hitbox.colliderect(coin.hitbox):
+    if scores >= 3:
         finish = True
         text = font.render(WIN_TEXT, True, BLACK)
 
@@ -15,4 +15,31 @@ def win_lose(coin, player, enemy):
         finish = True
         text = font.render(LOSE_TEXT, True, BLACK)
 
+    if player.hitbox.y > WINDOW_WIDTH - 10:
+        finish = True
+        text = font.render(LOSE_TEXT, True, BLACK)
+
     return finish, text
+
+
+def collisions(player, coins, scores):
+
+    for coin in coins:
+        if player.hitbox.colliderect(coin.hitbox):
+            scores += 1
+            coins.remove(coin)
+
+    return scores
+
+def draw_platforms(window, platforms):
+
+    for platform in platforms:
+        window.blit(platform.image, (platform.hitbox.x, platform.hitbox.y))
+
+def draw_coins(window, coins):
+
+    for coin in coins:
+        window.blit(coin.image, (coin.hitbox.x, coin.hitbox.y))
+        
+
+        
--- platformer2/main.py
+++ platformer2/main.py
@@ -8,32 +8,52 @@ window = Window(WINDOW_WIDTH, WINDOW_HEIGHT, CAPTION, BLUE)
 
 player = Player(PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_X, PLAYER_Y, PLAYER_IMAGE_PATH, PLAYER_SPEED)
 
-platform1 = Sprite(PLAYER_WIDTH, PLAYER_HEIGHT, 
+platform1 = Sprite(PLATFORM_WIDTH, PLATFORM_HEIGHT, 
                    PLATFORM1_X, PLATFORM1_Y, 
                    PLATFORM_IMAGE_PATH, PLATFORM_SPEED)
 
-platform2 = Sprite(PLAYER_WIDTH, PLAYER_HEIGHT, 
+platform2 = Sprite(PLATFORM_WIDTH, PLATFORM_HEIGHT, 
                    PLATFORM2_X, PLATFORM2_Y, 
                    PLATFORM_IMAGE_PATH, PLATFORM_SPEED)
 
-platform3 = Sprite(PLAYER_WIDTH, PLAYER_HEIGHT, 
+platform3 = Sprite(PLATFORM_WIDTH, PLATFORM_HEIGHT, 
                    PLATFORM3_X, PLATFORM3_Y, 
                    PLATFORM_IMAGE_PATH, PLATFORM_SPEED)
 
-platform4 = Sprite(PLAYER_WIDTH, PLAYER_HEIGHT, 
+platform4 = Sprite(PLATFORM4_WIDTH, PLATFORM4_HEIGHT, 
                    PLATFORM4_X, PLATFORM4_Y, 
                    PLATFORM_IMAGE_PATH, PLATFORM_SPEED)
 
-platform5 = Sprite(PLAYER_WIDTH, PLAYER_HEIGHT, 
+platform5 = Sprite(PLATFORM_WIDTH, PLATFORM_HEIGHT, 
                    PLATFORM5_X, PLATFORM5_Y, 
                    PLATFORM_IMAGE_PATH, PLATFORM_SPEED)
 
+coin1 = Sprite(COIN_WIDTH, COIN_HEIGHT, 
+                   COIN1_X, COIN1_Y, 
+                   COIN_IMAGE_PATH, COIN_SPEED)
+
+coin2 = Sprite(COIN_WIDTH, COIN_HEIGHT, 
+                   COIN2_X, COIN2_Y, 
+                   COIN_IMAGE_PATH, COIN_SPEED)
+
+coin3 = Sprite(COIN_WIDTH, COIN_HEIGHT, 
+                   COIN3_X, COIN3_Y, 
+                   COIN_IMAGE_PATH, COIN_SPEED)
+
+
+enemy = Sprite(ENEMY_WIDTH, ENEMY_HEIGHT, 
+                   ENEMY_X, ENEMY_Y, 
+                   ENEMY_IMAGE_PATH, ENEMY_SPEED)
+
+
 platforms = [platform1, platform2, platform3, platform4, platform5]
+coins = [coin1, coin2, coin3]
 
 font = pygame.font.Font(None, 40)
 game = True
 finish = False
 text = ""
+scores = 0
 
 while game:
     for event in pygame.event.get():
@@ -41,21 +61,21 @@ while game:
             game = False
 
     if not finish:
-        counter_text = font.render(f"Бали: {scores[0]}")
+        scores_text = font.render(f"Бали: {scores}", True, BLACK)
 
         window.screen.blit(player.image, (player.hitbox.x, player.hitbox.y))
+        window.screen.blit(enemy.image, (enemy.hitbox.x, enemy.hitbox.y))
 
-        window.screen.blit(platform1.image, (platform1.hitbox.x, platform1.hitbox.y))
-        window.screen.blit(platform2.image, (platform2.hitbox.x, platform2.hitbox.y))
-        window.screen.blit(platform3.image, (platform3.hitbox.x, platform3.hitbox.y))
-        window.screen.blit(platform4.image, (platform4.hitbox.x, platform4.hitbox.y))
-        window.screen.blit(platform5.image, (platform5.hitbox.x, platform5.hitbox.y))
-
-        window.screen.blit(counter_text, (100, 100))
+        draw_platforms(window.screen, platforms)
+        draw_coins(window.screen, coins)
+        
+        window.screen.blit(scores_text, (100, 100))
 
         player.controls(pygame.K_SPACE, pygame.K_a, pygame.K_d, platforms)
         player.gravity(platforms)
 
+        scores = collisions(player, coins, scores)
+        finish, text = win_lose(player, enemy, scores)
     else:
         window.screen.blit(text, (WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 - 50))
 
