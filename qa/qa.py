3d1183dba3e6ba2a1f6b7b1b0fbf3bcd0f986ac3
Mark <mark130599@gmail.com>
2023-07-29 16:50:22 +0300

    final version

--- qa/qa.py
+++ qa/qa.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from random import randint
 
 # запускається модуль зі шрифтами
 pg.font.init()
@@ -19,11 +20,13 @@ BUTTON2_Y = 240
 SHIFT_X = 10
 SHIFT_Y = 10
 
-QUESTION_TITLES = 'Питання'
-ANS_TITLES = 'Відповідь'
+QUESTION_TITLES = ["Питання", "Франції", "Програмування"]
+ANS_TITLES = ["Відповідь", "Париж", "Python"]
 CAPTION = "Question-Answer"
 FONT = None
 TEXT_SIZE = 60
+
+FPS = 60
 
 class Window():
     
@@ -44,6 +47,7 @@ class Button():
 
         self.rect = pg.Rect(x, y, width, height)
         self.color = color
+        self.text_color = text_color
 
         self.font = pg.font.Font(font, text_size)
         self.text = self.font.render(text, True, text_color)
@@ -56,7 +60,15 @@ class Button():
 
         screen.blit(self.text, (x, y))
 
+    def set_text(self, text):
+        self.text = self.font.render(text, True, self.text_color)
+
 class Game():
+
+    def __init__(self):
+        self.create_objects()
+        self.draw_objects()
+        self.game_loop()
 
     def create_objects(self):
         self.window = Window(WINDOW_WIDTH, WINDOW_HEIGHT, CAPTION, WHITE)
@@ -65,7 +77,7 @@ class Game():
             BUTTON_WIDTH, BUTTON_HEIGHT,
             BUTTON_X, BUTTON1_Y,
             PURPLE,
-            QUESTION_TITLES, TEXT_SIZE, FONT, BLACK
+            QUESTION_TITLES[0], TEXT_SIZE, FONT, BLACK
         )
 
 
@@ -73,15 +85,39 @@ class Game():
             BUTTON_WIDTH, BUTTON_HEIGHT,
             BUTTON_X, BUTTON2_Y,
             PURPLE,
-            ANS_TITLES, TEXT_SIZE, FONT, BLACK
+            ANS_TITLES[0], TEXT_SIZE, FONT, BLACK
         )
 
     def draw_objects(self):
         self.question_button.draw(self.window.screen)
         self.answer_button.draw(self.window.screen)
 
+    def event_listener(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.game = False
+            if event.type == pg.KEYDOWN and event.key == pg.K_q:
+                num = randint(0, len(QUESTION_TITLES) - 1)
+                text = QUESTION_TITLES[num]
+                self.question_button.set_text(text)
+
+            if event.type == pg.KEYDOWN and event.key == pg.K_a:
+                num = randint(0, len(QUESTION_TITLES) - 1)
+                text = ANS_TITLES[num]
+                self.answer_button.set_text(text)
+        
+
+    def update_window(self):
+        self.window.clock.tick(FPS)
+        pg.display.flip()
+
     def game_loop(self):
         self.game = True
-
+        
         while self.game:
-            pass
+            self.event_listener()
+            self.draw_objects()
+            self.update_window()
+
+
+game = Game()
