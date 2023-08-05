7ac5f164705f12c5ac6abb485bb5ae50130eaa32
Mark <mark130599@gmail.com>
2023-08-05 11:51:11 +0300

    memo_card, lesson 2

--- memory_card2/ui/BaseScreen.py
+++ memory_card2/ui/BaseScreen.py
@@ -1,4 +1,13 @@
-from PyQt5.QtWidgets import QWidget
+from utils.Constants import*
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QPushButton,
+    QSpinBox,
+    QLabel,
+    QHBoxLayout,
+    QVBoxLayout,
+    QWidget
+)
 
 class BaseScreen(QWidget):
 
@@ -6,4 +15,47 @@ class BaseScreen(QWidget):
         super().__init__()
 
         self.resize(width, height)
-        self.setWindowTitle(title) 
+        self.setWindowTitle(title)
+    
+    def createWidgets(self):
+
+        self.menuButton = QPushButton(MENU_BUTTON_TEXT)
+        self.restButton = QPushButton(REST_BUTTON_TEXT)
+        self.restSpinBox = QSpinBox()
+        self.restSpinBox.setValue(START_MINUTES)
+        self.minutesLabel = QLabel(MINUTES_TEXT)
+
+        self.questionLabel = QLabel()
+
+        self.answerButton = QPushButton()
+
+    def createLayouts(self):
+    
+        self.row1 = QHBoxLayout()
+        self.row2 = QHBoxLayout()
+        self.row3 = QHBoxLayout()
+        self.row4 = QHBoxLayout()
+
+        self.mainColumn = QVBoxLayout()
+
+    def setMainLayout(self):
+        self.row1.addWidget(self.menuButton)
+        self.row1.addStretch(1)
+        self.row1.addWidget(self.restButton)
+        self.row1.addWidget(self.restSpinBox)
+        self.row1.addWidget(self.minutesLabel)
+
+        self.row2.addWidget(self.questionLabel, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+
+        self.row4.addStretch(1)
+        self.row4.addWidget(self.answerButton, stretch=2)
+        self.row4.addStretch(1)
+
+        self.mainColumn.addLayout(self.row1, stretch=1)
+        self.mainColumn.addLayout(self.row2, stretch=2)
+        self.mainColumn.addStretch(1)
+        self.mainColumn.addLayout(self.row3, stretch=8)
+        self.mainColumn.addStretch(1)
+        self.mainColumn.addLayout(self.row4, stretch=1)
+
+        self.setLayout(self.mainColumn)
