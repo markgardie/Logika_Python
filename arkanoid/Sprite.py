7bcf580e98ebb47bcbf71afaecaa4d07298727fd
Mark <mark130599@gmail.com>
2023-08-12 16:25:20 +0300

    arkanoid, lesson 1

---
+++ arkanoid/Sprite.py
@@ -0,0 +1,9 @@
+import pygame as pg
+
+class Sprite():
+
+    def __init__(self, x, y, width, height, image_path):
+        self.hitbox = pg.Rect(x, y, width, height)
+
+        self.image = pg.image.load(image_path)
+        self.image = pg.transform.scale(self.image, (x, y))
