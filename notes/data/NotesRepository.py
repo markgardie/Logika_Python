f4d478b1c3c7e02c77922e1445be86b9765a8692
Mark <mark130599@gmail.com>
2023-09-02 11:35:57 +0300

    notes, lesson 1

---
+++ notes/data/NotesRepository.py
@@ -0,0 +1,31 @@
+from domain.Note import Note
+
+class NotesRepository():
+
+    def __init__(self):
+
+        self.notes = []
+
+    def createNote(self):
+        pass
+
+    def deleteNote(self):
+        pass
+
+    def updateNote(self):
+        pass
+
+    def readNotes(self):
+        pass
+
+    def addTag(self):
+        pass
+
+    def deleteTag(self):
+        pass
+
+    def readTags(self):
+        pass
+
+    def searchByTag(self):
+        pass
---
+++ notes/domain/Note.py
@@ -0,0 +1,8 @@
+
+class Note():
+
+    def __init__(self, name, text = "", tags = []):
+
+        self.name = name
+        self.text = text
+        self.tags = tags
