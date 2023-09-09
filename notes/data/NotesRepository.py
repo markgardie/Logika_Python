948659b20841b92fc39d3dd6410c7b747b117a96
Mark <mark130599@gmail.com>
2023-09-09 17:24:26 +0300

    notes, lesson 2

--- notes/data/NotesRepository.py
+++ notes/data/NotesRepository.py
@@ -1,30 +1,37 @@
-from domain.Note import Note
+import json
 
 class NotesRepository():
 
     def __init__(self):
-
         self.notes = []
+        self.path = r"D:\Mark\Desktop\Logika_Python\notes\data\notes.json"
 
-    def createNote(self):
+    def saveFile(self):
+        with open(self.path, "w", encoding="utf-8") as file:
+            json.dump(self.notes)
+
+    def createNotes(self, newNote):
+        self.notes.append(newNote.__dict__)
+        self.saveFile()
+        pass
+
+    def readNotes(self):
+        with open(self.path, "r", encoding="utf-8") as file:
+            self.notes = file.read()
+
+    def updateNote(self):
         pass
 
     def deleteNote(self):
         pass
 
-    def updateNote(self):
-        pass
-
-    def readNotes(self):
-        pass
-
     def addTag(self):
         pass
 
-    def deleteTag(self):
+    def readTags(self):
         pass
 
-    def readTags(self):
+    def deleteTag(self):
         pass
 
     def searchByTag(self):
