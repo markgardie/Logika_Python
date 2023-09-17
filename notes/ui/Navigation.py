5f61059730e0f2f06f9ed5ee839c34cc7d0bd84c
Mark <mark130599@gmail.com>
2023-09-17 13:34:39 +0300

    notes, lesson 4

--- notes/ui/Navigation.py
+++ notes/ui/Navigation.py
@@ -1,19 +1,35 @@
 from ui.MainScreen import Ui_MainScreen
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QInputDialog
+from data.NotesRepository import NotesRepository
 
 class Navigation():
 
     def __init__(self):
 
-        self.mainScreen = QWidget()
-        mainScreenSetup = Ui_MainScreen()
-        mainScreenSetup.setupUi(self.mainScreen)
+        self.mainWidget = QWidget()
+        self.mainScreen = Ui_MainScreen()
+        self.mainScreen.setupUi(self.mainWidget)
 
-        self.mainScreen.show()
+        self.repository = NotesRepository()
 
     def setListeners(self):
 
-        pass
+        self.mainScreen.createNoteBtn.clicked.connect(self.createNote)
+
+    def showNotesList(self):
+        for note in self.repository.notes:
+            self.mainScreen.notesListWidget.addItem(note["title"])
+
+    def showText(self):
+        noteTitle = self.mainScreen.notesListWidget.selectedItems()[0].text
+        for note in self.repository.notes:
+            if  note["title"] == noteTitle:
+                noteText = note["text"]
+
+        self.mainScreen.noteTextEdit.setText(noteText)
+
+    def createNote(self):
+        noteTitle, ok = QInputDialog.getText(self.mainScreen, "Додати нотатку", "Введіть нотатку")
 
 
         
