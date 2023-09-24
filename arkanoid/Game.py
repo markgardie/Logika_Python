2874581018a5534f5151b5bce63a47466a6ba868
Mark <mark130599@gmail.com>
2023-09-24 17:29:01 +0300

    arkanoid, lesson 3

--- arkanoid/Game.py
+++ arkanoid/Game.py
@@ -3,6 +3,7 @@ from Sprite import*
 from Window import Window
 from Platform import Platform
 from Ball import Ball
+from random import randint
 
 class Game():
 
@@ -20,26 +21,65 @@ class Game():
         self.blocks = self.create_blocks()
 
     def create_blocks(self):
+
+        x = 20
+        y = 20
+
         for row in MAP:
             for place in row:
                 if place == "1":
-                    pass #TODO
+                    block = Sprite(BLOCK_WIDTH, BLOCK_HEIGHT,
+                                   x, y, BLOCK_IMAGE_PATH)
+                x += 76
+            y += 56
+            x = 20        
 
     def draw_objects(self):
-        pass
+        self.window.screen.blit(self.platform.image, 
+                                (self.platform.hitbox.x, self.platform.hitbox.y))
+        
+        self.window.screen.blit(self.ball.image, 
+                                (self.ball.hitbox.x, self.ball.hitbox.y))
+        
+        for block in self.blocks:
+            self.window.screen.blit(self.block.image, 
+                                (self.block.hitbox.x, self.block.hitbox.y))
 
     def move_objects(self):
-        pass
+
+        self.dir_x = 1
+        self.dir_y = -1
+
+        self.platform.controls(pg.K_a, pg.K_d)
+        self.ball.move(self.dir_x, self.dir_y)
 
     def collisions(self):
-        pass
+        
+        if self.ball.hitbox.colliderect(self.platform.hitbox):
+            self.dir_y = -1
+            self.dir_x = randint(-1, 1)
 
+        for block in self.blocks:
+            if self.ball.hitbox.colliderect(self.block.hitbox):
+                self.dir_y = 1
+                self.dir_x = randint(-1, 1)
+                self.blocks.remove(block)
+
+        if self.ball.hitbox.x < 0:
+            self.dir_x = 1  
 
     def win_lose(self):
-        pass
+        font = pg.font.Font(None, FONT_SIZE)
+
+        if len(self.blocks) == 0:
+            self.finish = True
+            self.text = font.render(WIN_TEXT, True, BLACK)
+
+        
 
     def update_window(self):
-        pass
+        self.window.clock.tick(FPS)
+        pg.display.update()
 
     def game_loop(self):
         self.create_objects()
