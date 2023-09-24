40646728e95c0be7d476264d7cbe51dba3d9e063
Mark <mark130599@gmail.com>
2023-09-24 11:50:59 +0300

    tanks controls

--- tanks/Tank.py
+++ tanks/Tank.py
@@ -5,6 +5,22 @@ from Bullet import Bullet
 
 class Tank(Sprite):
 
+    def controls(self, w, a, s, d):
+
+        key_pressed = pg.key.get_pressed()
+
+        if key_pressed [w] and self.hitbox.y > 0: 
+            self.hitbox.y -= TANK_SPEED
+
+        if key_pressed [a] and self.hitbox.x > 0:
+            self.hitbox.x -= TANK_SPEED
+    
+        if key_pressed [s] and self.hitbox.y < WINDOW_HEIGHT:
+            self.hitbox.y += TANK_SPEED
+    
+        if key_pressed [d] and self.hitbox.x < WINDOW_WIDTH:
+            self.hitbox.x += TANK_SPEED
+
 
     def fire1(self, bullets1):
         bullet = Bullet(
