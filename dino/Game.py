3ce06a28a9564981cd6b2c5a1f006c2b9944502c
Mark <mark130599@gmail.com>
2023-09-30 13:33:41 +0300

    final, lesson 3

--- dino/Game.py
+++ dino/Game.py
@@ -1,51 +1,68 @@
 from Window import Window
 from Sprite import Sprite
-from Platform import Platform
-from Ball import Ball
+from Dino import Dino
+from Obstacle import Obstacle
 from Constants import*
 import pygame as pg
 from random import randint
 
+
 class Game():
 
     def create_objects(self):
+        self.dino = Dino(
+            DINO_X, DINO_Y,
+            DINO_WIDTH, DINO_HEIGHT,
+            DINO_IMAGE_PATH
+        )
 
-        self.window = Window(WINDOW_WIDTH, WINDOW_HEIGHT, CAPTION, BLUE)
+    def create_obstacles(self):
+        self.obstacle = None
 
-        self.platform = Platform(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 30, 
-                                 PLATFORM_WIDTH, PLATFORM_HEIGHT,
-                                 PLATFORM_IMAGE_PATH)
-        
-        self.ball = Ball(WINDOW_WIDTH / 2, WINDOW_HEIGHT /2, 
-                                 BALL_WIDTH, BALL_HEIGHT,
-                                 BALL_IMAGE_PATH)
-        
-        self.blocks = self.create_blocks()
+        if self.obstacle == None:
+            obstacle_type = randint(1, 2)
+
+            if obstacle_type == 1:
+                self.obstacle = Obstacle(
+                    CACTUS_X, CACTUS_Y,
+                    CACTUS_WIDTH, CACTUS_HEIGHT,
+                    CACTUS_IMAGE_PATH, OBSTACLE_SPEED
+                )
+
+            if obstacle_type == 2:
+                self.obstacle = Obstacle(
+                    BAT_X, BAT_Y,
+                    BAT_WIDTH, BAT_HEIGHT,
+                    BAT_IMAGE_PATH, OBSTACLE_SPEED
+                )
+
+    def destroy_obstacles(self):
+        if self.obstacle.hitbox.x < 0:
+            self.obstacle = None
+
+    def draw_objects(self):
+        pass
+
+    def move_objects(self):
+        pass
+
+    def collisions(self):
+        pass
+
+    def win_lose(self):
+        pass
+
     
-    def create_blocks(self):
-        blocks = []
+    def event_handler(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.game = False
 
-        x = 20
-        y = 20
-
-        for row in MAP:
-            for place in row:
-                if place == "1":
-                    block = Sprite(x, y, BLOCK_WIDTH, BLOCK_HEIGHT, BLOCK_IMAGE_PATH)
-                    blocks.append(block)
-
-                x += 76
-
-            y += 56
-            x = 20
-
-        
-        return blocks
-    
     def game_loop(self):
-
         self.game = True
         self.finish = False
+
+        self.create_objects()
 
         while self.game:
             if self.finish: 
@@ -56,53 +73,3 @@ class Game():
                 self.collisions()
                 self.event_handler()
                 self.win_lose()
-
-    def draw_objects(self):
-        self.window.screen.blit(self.platform.image, (self.platform.hitbox.x, self.platform.hitbox.y))
-        self.window.screen.blit(self.ball.image, (self.ball.hitbox.x, self.ball.hitbox.y))
-
-        for block in self.blocks:
-            self.window.screen.blit(block.image, (block.hitbox.x, block.hitbox.y))
-
-    def move_objects(self):
-
-        self.dir_x = 1
-        self.dir_y = -1
-
-        self.platform.controls(pg.K_a, pg.K_d)
-        self.ball.move(self.dir_x, self.dir_y)
-
-    def collisions(self):
-        
-        # ball, platform
-        if self.ball.hitbox.colliderect(self.platform.hitbox):
-            self.dir_y = -1
-            self.dir_x = randint(-1, 1)
-
-        # ball, blocks
-        for block in self.blocks:
-            if self.ball.hitbox.colliderect(self.block.hitbox):
-                self.dir_y = 1
-                self.dir_x = randint(-1, 1)
-
-        # ball, left border
-        if self.ball.hitbox.x < 0:
-            self.dir_x = 1
-
-    def event_handler(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                self.game = False
-
-    def win_lose(self):
-
-        font = pg.font.Font(None, FONT_SIZE)
-
-        if len(self.blocks) == 0:
-            self.finish = True
-            self.final_text = font.render(WIN_TEXT, True, BLACK)
-
-        if self.ball.hitbox.y > WINDOW_HEIGHT:
-            self.finish = True
-            self.final_text = font.render(LOSE_TEXT, True, BLACK)
-
--- tanks/Game.py
+++ tanks/Game.py
@@ -1,51 +1,74 @@
 from Window import Window
 from Sprite import Sprite
-from Platform import Platform
-from Ball import Ball
+from Tank import Tank
+from Bullet import Bullet
 from Constants import*
 import pygame as pg
 from random import randint
+
 
 class Game():
 
     def create_objects(self):
 
-        self.window = Window(WINDOW_WIDTH, WINDOW_HEIGHT, CAPTION, BLUE)
+        self.window = Window(
+            WINDOW_WIDTH, WINDOW_HEIGHT,
+            CAPTION, BLUE
+        )
 
-        self.platform = Platform(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 30, 
-                                 PLATFORM_WIDTH, PLATFORM_HEIGHT,
-                                 PLATFORM_IMAGE_PATH)
+
+        self.tank1 = Tank(
+            TANK1_X, TANK1_Y,
+            TANK_WIDTH, TANK_HEIGHT,
+            TANK_IMAGE_PATH
+        )
+
+        self.tank2 = Tank(
+            TANK2_X, TANK2_Y,
+            TANK_WIDTH, TANK_HEIGHT,
+            TANK_IMAGE_PATH
+        )
+
+        self.wall1 = Sprite(V_WALL_WIDTH, V_WALL_HEIGHT, 
+                       WALL1_X, WALL1_Y, 
+                       WALL_IMAGE_PATH, WALL_SPEED)
         
-        self.ball = Ball(WINDOW_WIDTH / 2, WINDOW_HEIGHT /2, 
-                                 BALL_WIDTH, BALL_HEIGHT,
-                                 BALL_IMAGE_PATH)
+        self.wall2 = Sprite(V_WALL_WIDTH, V_WALL_HEIGHT, 
+                       WALL2_X, WALL2_Y, 
+                       WALL_IMAGE_PATH, WALL_SPEED)
+
+        self.wall3 = Sprite(H_WALL_WIDTH, H_WALL_HEIGHT, 
+                       WALL3_X, WALL3_Y, 
+                       WALL_IMAGE_PATH, WALL_SPEED)
         
-        self.blocks = self.create_blocks()
+        self.wall4 = Sprite(H_WALL_WIDTH, H_WALL_HEIGHT, 
+                       WALL4_X, WALL4_Y, 
+                       WALL_IMAGE_PATH, WALL_SPEED)
+
+
+    def draw_objects(self):
+        pass
+
+    def move_objects(self):
+        
+
+    def collisions(self):
+        pass
+
+    def win_lose(self):
+        pass
+
     
-    def create_blocks(self):
-        blocks = []
+    def event_handler(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.game = False
 
-        x = 20
-        y = 20
-
-        for row in MAP:
-            for place in row:
-                if place == "1":
-                    block = Sprite(x, y, BLOCK_WIDTH, BLOCK_HEIGHT, BLOCK_IMAGE_PATH)
-                    blocks.append(block)
-
-                x += 76
-
-            y += 56
-            x = 20
-
-        
-        return blocks
-    
     def game_loop(self):
-
         self.game = True
         self.finish = False
+
+        self.create_objects()
 
         while self.game:
             if self.finish: 
@@ -56,53 +79,3 @@ class Game():
                 self.collisions()
                 self.event_handler()
                 self.win_lose()
-
-    def draw_objects(self):
-        self.window.screen.blit(self.platform.image, (self.platform.hitbox.x, self.platform.hitbox.y))
-        self.window.screen.blit(self.ball.image, (self.ball.hitbox.x, self.ball.hitbox.y))
-
-        for block in self.blocks:
-            self.window.screen.blit(block.image, (block.hitbox.x, block.hitbox.y))
-
-    def move_objects(self):
-
-        self.dir_x = 1
-        self.dir_y = -1
-
-        self.platform.controls(pg.K_a, pg.K_d)
-        self.ball.move(self.dir_x, self.dir_y)
-
-    def collisions(self):
-        
-        # ball, platform
-        if self.ball.hitbox.colliderect(self.platform.hitbox):
-            self.dir_y = -1
-            self.dir_x = randint(-1, 1)
-
-        # ball, blocks
-        for block in self.blocks:
-            if self.ball.hitbox.colliderect(self.block.hitbox):
-                self.dir_y = 1
-                self.dir_x = randint(-1, 1)
-
-        # ball, left border
-        if self.ball.hitbox.x < 0:
-            self.dir_x = 1
-
-    def event_handler(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                self.game = False
-
-    def win_lose(self):
-
-        font = pg.font.Font(None, FONT_SIZE)
-
-        if len(self.blocks) == 0:
-            self.finish = True
-            self.final_text = font.render(WIN_TEXT, True, BLACK)
-
-        if self.ball.hitbox.y > WINDOW_HEIGHT:
-            self.finish = True
-            self.final_text = font.render(LOSE_TEXT, True, BLACK)
-
