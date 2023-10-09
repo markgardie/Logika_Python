f26e09b04e06bfe05c3dc826ddd4a541c170cc3e
Mark <mark130599@gmail.com>
2023-10-09 14:47:36 +0300

    fast clicker, lesson 2

--- fast_clicker/Game.py
+++ fast_clicker/Game.py
@@ -1,13 +1,51 @@
-
+from Window import Window
+from Sprite import Sprite
+from Label import Label
+from Card import Card
+from Constants import*
+import pygame as pg
+from random import randint
 
 class Game():
 
     def create_objects(self):
-       pass
 
+        self.window = Window(
+            WINDOW_WIDTH, WINDOW_HEIGHT,
+            CAPTION, BLUE
+        )
+
+
+        self.cards = self.create_cards()
+
+    def create_cards(self):
+        cards = []
+
+        x = START_X
+
+        for i in range(CARDS_NUM):
+            
+            new_card = Card(
+                CARD_WIDTH, CARD_HEIGHT,
+                x, CARDS_Y, YELLOW
+            )
+
+            cards.append(new_card)
+
+            x += CARDS_DISTANCE
+
+        return cards
 
     def draw_objects(self):
-        pass
+        self.draw_cards()
+
+    def draw_cards(self):
+
+        card_num = randint(0, len(self.cards) - 1)
+
+        for card in self.cards:
+            pg.draw.rect(self.window.screen, card.color, card.hitbox)
+
 
     def move_objects(self):
 
@@ -15,7 +53,13 @@ class Game():
 
     def collisions(self):
         
-       pass
+       self.click()
+
+    def click(self, e):
+        x, y = e.pos
+        for card in self.cards:
+            if card.hitbox.collidepoint(x, y):
+                pass``
 
     def win_lose(self):
        pass
