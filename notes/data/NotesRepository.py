32b1e39192f3980304cf7f463eb7fa6780ec1918
Mark <mark130599@gmail.com>
2023-11-01 15:14:23 +0200

    notes app, comments

--- notes/data/NotesRepository.py
+++ notes/data/NotesRepository.py
@@ -2,61 +2,104 @@ import json
 
 class NotesRepository():
 
+    # конструктор
+    # виконуються початкові налаштування репозиторія
     def __init__(self):
+        # збереження даних в списку (тимчасове)
         self.notes = []
+        # шлях до файлу для постійного збереження
         self.path = r"D:\Mark\Desktop\Logika_Python\notes\data\notes.json"
 
+        # нюанси: r-строка для ігнорування слешів (\) і керуючих символів
+        # список зберігає дані тимчасово, але зручний в роботі
+        # файл зберігає дані постійно, але незручний в роботі
+
+    # збереження нових даних у файлі
     def saveFile(self):
+        # відкриття файлу
+        # завантаження списку у файл
         with open(self.path, "w", encoding="utf-8") as file:
             json.dump(self.notes, file)
 
+        # нюанси: обережно з режимами відкриття файла: тільки читання чи переписування
+
+    # створення нотатки
     def createNote(self, newNote):
+        # нова нотатка додається в список
         self.notes.append(newNote)
         self.saveFile()
-        pass
 
+        # нюанси: нотатка являється словником
+        # notes - це по суті список словників
+
+
+    # читання нотаток із файла
     def readNotes(self):
+        # відкриття файла і збереження даних в список
         with open(self.path, "r", encoding="utf-8") as file:
             self.notes = json.load(file)
 
+    # оновлення тексту нотатки
     def updateNote(self, noteTitle, noteText):
+        # перебирання списку нотаток
+        # пошук нотатки по заголовоку (назві)
         for note in self.notes:
             if note["title"] == noteTitle:
                 note["text"] = noteText
-        
+
+        # збереження змін у файл
         self.saveFile()
 
+    # видалення нотатки
     def deleteNote(self, noteTitle):
+        # перебирання списку нотаток
+        # пошук нотатки по заголовоку (назві)
         for note in self.notes:
             if note["title"] == noteTitle:
                 self.notes.remove(note)
 
+        # збереження змін у файл
         self.saveFile()
 
+    # додавання тегу
     def addTag(self, noteTitle, tag):
+        # шукаємо нотатку, в яку треба цей тег додати
         for note in self.notes:
             if note["title"] == noteTitle:
                 note["tags"].append(tag)
 
+        # зберігаємо зміни
         self.saveFile()
 
+    # отримання списку тегів певної нотатки
     def readTags(self, noteTitle):
+        # шукаємо нотатку
+        # у неї отримуємо список тегів
         for note in self.notes:
             if note["title"] == noteTitle:
                 return note["tags"]
     
+    # видалення тегів
     def deleteTag(self, noteTitle, tag):
+        # шукаємо нотатку, в якій треба видалити тег
+        # видаляємо тег
         for note in self.notes:
             if note["title"] == noteTitle:
                 note["tags"].remove(tag)
 
         self.saveFile()
 
+    # пошук нотаток по тегам
     def searchByTag(self, searchTag):
+        # список нотаток, які мають шуканий тег
         filteredNotes = []
+
+        # перебираємо список нотаток
+        # і забираємо (фільтруємо) тільки ті, що мають шуканий тег
         for note in self.notes:
             for tag in note["tags"]:
                 if tag == searchTag:
                     filteredNotes.append(note)
 
+        # повертаємо відфільтровані нотатки
         return filteredNotes
--- notes/ui/Navigation.py
+++ notes/ui/Navigation.py
@@ -4,50 +4,88 @@ from data.NotesRepository import NotesRepository
 
 class Navigation():
 
+    # конструктор
+    # початкові налаштунки
     def __init__(self):
 
+
+        # створення та налаштування вікна
         self.mainWidget = QWidget()
         self.mainScreen = Ui_MainScreen()
         self.mainScreen.setupUi(self.mainWidget)
 
+        # показ вікна
         self.mainWidget.show()
 
+        # створення репозиторія
         self.repository = NotesRepository()
+
+        # запуск інших функцій
         self.showNotesList()
         self.setListeners()
 
+    # налаштування слухачів, які слідкуються за кліками на кнопки
     def setListeners(self):
 
+        # кнопки і кліки на кнопки, відповідальні за нотатки
         self.mainScreen.createNoteBtn.clicked.connect(self.createNote)
         self.mainScreen.deleteNoteBtn.clicked.connect(self.deleteNote)
         self.mainScreen.saveNoteBtn.clicked.connect(self.saveNote)
 
+        # кнопки і кліки на кнопки, відповідальні за теги
         self.mainScreen.addTagBtn.clicked.connect(self.addTag)
         self.mainScreen.deleteTagBtn.clicked.connect(self.deleteTag)
         self.mainScreen.searchBtn.clicked.connect(self.search)
 
+        # вибір певної нотатки в списку нотаток
         self.mainScreen.notesListWidget.itemClicked.connect(self.showNoteInfo)
 
+    # показ списку нотаток
     def showNotesList(self):
+        # отримуємо у репозиторія список нотаток
         self.repository.readNotes()
+
+        # додаємо всі нотатки в ListWidget
         for note in self.repository.notes:
             self.mainScreen.notesListWidget.addItem(note["title"])
 
+        # нюанси: розповісти про ListWidget
+        # це віджет, який дозволяє виводити на екран список елементів,
+        # а також клікати  на кожен елемент
+
+    # показ вмісту нотаток: тексту, тегів
     def showNoteInfo(self):
+        # дізнаємось назву нотатки, яка зараз обрана
         noteTitle = self.mainScreen.notesListWidget.selectedItems()[0].text()
+
+
         for note in self.repository.notes:
+
+            # отримуємо текст нотатки
+            # і виводимо його в TextEdit на екран
             if  note["title"] == noteTitle:
                 noteText = note["text"]
                 self.mainScreen.noteTextEdit.setText(noteText)
                 
+                # отримуємо теги нотатки
+                # додаємо їх в ListWidget тегів
                 tags = note["tags"]
                 self.mainScreen.tagsListWidget.clear()
 
                 for tag in tags:
                     self.mainScreen.tagsListWidget.addItem(tag)
 
+            # нюанси: розповісти про TextEdit
+            # Це велике поле, в яке можна вписати багато тексту
+            # LineEdit - це поле тільки в одну строку
+
+    # створення нотатки
     def createNote(self):
+        # запускаємо діалогове вікно, в якому запитуємо назву нотатки
         noteTitle, ok = QInputDialog.getText(self.mainWidget, "Додати нотатку", "Введіть нотатку")
+        
+        # створюємо нову нотатку
+        # текст та теги поки пусті
         if ok and noteTitle != "":
             note = {
                 "title": noteTitle,
@@ -55,51 +93,94 @@ class Navigation():
                 "tags": []
             }
 
+            # додаємо нову нотатку в репозиторій
             self.repository.createNote(note)
             self.mainScreen.notesListWidget.addItem(noteTitle)
 
+        # нюанси: важливо перевіряти чи правильно відпрацював QInputDialog, 
+        # бо інакше може створитись пуста нотатка
+        # розповісти про QInputDialog
+        # це віджет, який запускає маленьке діалогове вікно із полем для вводу
+
+    # зберігаємо новий текст нотатки
     def saveNote(self):
+        # отримуємо назву та текст нотатки
         noteTitle = self.mainScreen.notesListWidget.selectedItems()[0].text()
         noteText = self.mainScreen.noteTextEdit.toPlainText()
         
         if noteTitle != "":   
+            # оновлюємо нотатку
             self.repository.updateNote(noteTitle, noteText)
 
+        # нюанси: розповісти про selectedItems
+        # ця функція повертає список обраниїх елементів
+        # але користувач обирає тільки один елемент, тому нам треба тільки перший елемент списка
+        # тому ми пишемо [0]
+
+    # видалення нотатки
     def deleteNote(self):
+        # отримуємо назву нотатки, яку треба видалити
         noteTitle = self.mainScreen.notesListWidget.selectedItems()[0].text()
         
         if noteTitle != "":
+            # видаляємо нотатку
+            # очищаємо список нотаток і поле з текстом
+            # і заново завантажуємо оновлені нотатки
             self.repository.deleteNote(noteTitle)
             self.mainScreen.notesListWidget.clear()
             self.mainScreen.noteTextEdit.clear()
 
             self.showNotesList()
 
+        # нюанси: після видалення нотатки, сама вона з екрана не пропаде
+        # нам треба це зробити вручну: очистити список нотаток і заново
+        # завантажити вже новий список без видаленої нотатки
 
+    # додати тег до нотатки
     def addTag(self):
+
+        # отримуємо назву нотатки, яка обрана
+        # отримуємо назву тегу
         noteTitle = self.mainScreen.notesListWidget.selectedItems()[0].text()
         tag = self.mainScreen.searchLineEdit.text()
         
         if tag != "":
+            # додаємо новий тег в репозиторій і на екран
             self.repository.addTag(noteTitle, tag)
             self.mainScreen.tagsListWidget.addItem(tag)
 
+    # видалення тега
     def deleteTag(self):
+        # дивимось назву нотатки і тег, який треба видалити
         noteTitle = self.mainScreen.notesListWidget.selectedItems()[0].text()
         tag = self.mainScreen.tagsListWidget.selectedItems()[0].text()
         
         if tag != "":
+            # видаляємо тег
             self.repository.deleteTag(noteTitle, tag)
 
             self.mainScreen.tagsListWidget.clear()
             self.showNoteInfo()
 
+        # нюанси: схоже на видалення нотатки - тег при видалення з екрану сам не пропаде
+        # треба очистити список тегів вручну і заново завантажити оновлений список тегів
+
+    # пошук нотаток по тегу
     def search(self):
+        # із поля пошуку отримуємо назву тегу, за яким будемо шукати нотатки
         tag = self.mainScreen.searchLineEdit.text()
         if tag != "":
+
+            # очищаємо список нотаток
+            # і з репозиторія отримуємо списко вже відфільтрованих нотаток
+            # показуємо на екрані
             self.mainScreen.notesListWidget.clear()
             for note in self.repository.searchByTag(tag):
                 self.mainScreen.notesListWidget.addItem(note["title"])
+        
+        # аби повернути на екран список всіх нотаток треба
+        # очистити поле пошуку і зробити тег пустим
+        # тоді завантажеться весь список
         else:
             self.mainScreen.notesListWidget.clear()
             self.showNotesList()
