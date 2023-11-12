3b6cd403ae39ab39f12b456285b56b862cd83220
Mark <mark130599@gmail.com>
2023-11-12 13:18:13 +0200

    flask, lesson 1

--- blog_flask/start.py
+++ blog_flask/start.py
@@ -9,3 +9,24 @@ def index():
 @app.route('/about_me')
 def about_me():
     return "My name is Marks"
+
+
+my_shelf = dict()
+
+while True:
+    author = input('Введіть автора (q - завершити):')
+    if author == 'q':
+        break
+    books = list()    
+    while True:
+        book = input('Введіть книгу (s - стоп):')
+        if book == 's':
+            break
+        books.append(book)
+    my_shelf[author] = books
+ 
+for author in my_shelf:
+   print(author, '-', my_shelf[author])
+
+
+
