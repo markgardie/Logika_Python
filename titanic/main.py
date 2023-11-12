26f6664d4028a5d568a7885508903f0d47c1e809
Mark <mark130599@gmail.com>
2023-11-12 15:31:28 +0200

    ml, lesson 2

--- titanic/main.py
+++ titanic/main.py
@@ -41,3 +41,14 @@ X = df.drop('Survived', axis=1)
 y = df['Survived']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+print("Точність:",  accuracy_score(y_test, y_pred))
