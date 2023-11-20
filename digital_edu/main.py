1019ed02f54fb35617ee7db7954c68b051d7b744
Mark <mark130599@gmail.com>
2023-11-20 14:43:42 +0200

    machine learning, homework tips

--- digital_edu/main.py
+++ digital_edu/main.py
@@ -11,7 +11,15 @@ def fill_form(form):
 
 df["education_form"] = df["education_form"].apply(fill_form, axis=1)
 
+
+print(df.value_counts(df["education_status"])) # передивитись скільки, які є рівні освіти
+
 def fill_status(status):
+    # для кожного рівня освіти визначити цифру
+    # Alumnus - означає випускник
+    # Student - діючий студент (ще не випустився)
+    # Undergraduate Applicant - вступник на рівень бакалавра
+    # Рівні освіти: спеціаліст, бакалавр, магістр, кандидат наук, доктор наук
     if status == "Alumnus (Specialist)":
         return 0
     if status == "Student (Bachelor's)":
@@ -20,6 +28,5 @@ def fill_status(status):
         return 2
     return 3
 
-df["education_form"] = df["education_form"].apply(fill_form, axis=1)
+df["education_status"] = df["education_status"].apply(fill_status, axis=1)
 
-print(df.groupby(["education_status"], axis=1))
