16d27c2585208d87d156652a50b8911145d798a4
markgardie <60879509+markgardie@users.noreply.github.com>
2024-01-30 12:49:18 +0200

    db, lesson 2, prepare files

---
+++ db_lesson2/db_scripts.py
@@ -0,0 +1,64 @@
+import sqlite3
+db_name = 'quiz.sqlite'
+conn = None
+curor = None
+
+def open():
+    global conn, cursor
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+def close():
+    cursor.close()
+    conn.close()
+
+def do(query):
+    cursor.execute(query)
+    conn.commit()
+
+def clear_db():
+    ''' видаляє всі таблиці '''
+    open()
+    query = '''DROP TABLE IF EXISTS quiz_content'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS question'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS quiz'''
+    do(query)
+    close()
+
+    
+def create():
+    pass
+
+def add_questions():
+    pass
+
+def add_quiz():
+    pass
+
+def add_links():
+    pass
+
+def get_question(question_id = 0, quiz_id=1):
+    pass
+
+def show(table):
+    query = 'SELECT * FROM ' + table
+    open()
+    cursor.execute(query)
+    print(cursor.fetchall())
+    close()
+
+def show_tables():
+    show('question')
+    show('quiz')
+    show('quiz_content')
+
+def main():
+    clear_db()
+    create()
+    show_tables()
+
+if __name__ == "__main__":
+    main()
