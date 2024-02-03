20776cd8669dc4a34cf29a12172ca10b037d9eb9
markgardie <60879509+markgardie@users.noreply.github.com>
2024-02-03 13:34:06 +0200

    notes, lesson 1

---
+++ notes/data/notes_dao.py
@@ -0,0 +1,23 @@
+import json
+
+class NotesDao():
+
+
+    def createNote(self):
+        pass
+
+    def getNotes(self):
+        pass
+
+    def updateNoteText(self):
+        pass
+
+    def deleteNote(self):
+        pass
+
+    def addTag(self):
+        pass
+
+    def deleteTag(self):
+        pass
+
