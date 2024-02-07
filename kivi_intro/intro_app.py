e8cd4448e58d43274342320d8ba847c59b6e3432
markgardie <60879509+markgardie@users.noreply.github.com>
2024-02-07 19:23:06 +0200

    kivi intro

---
+++ kivi_intro/intro_app.py
@@ -0,0 +1,4 @@
+from kivy.app import App
+from intro_screen_manager import intro_screen_manager
+
+
--- ping-pong/ball.py
+++
@@ -1,8 +0,0 @@
-from sprite import Sprite
-from constants import*
-
-class Ball(Sprite):
-
-    def move(self, dir_x, dir_y):
-        self.rect.x += BALL_SPEED * dir_x
-        self.rect.y += BALL_SPEED * dir_y
