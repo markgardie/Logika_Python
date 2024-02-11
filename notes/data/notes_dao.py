4ed577138429809391bd32027070d753ef8e07bd
markgardie <60879509+markgardie@users.noreply.github.com>
2024-02-11 12:05:05 +0200

    notes, lesson 2

--- notes/data/notes_dao.py
+++ notes/data/notes_dao.py
@@ -1,23 +1,59 @@
 import json
 
+path = "D:\Mark\Desktop\Logika_Python\notes\data\notes.json"
+
 class NotesDao():
 
 
-    def createNote(self):
-        pass
+    def createNote(self, title):
+        note = {
+            "title": title,
+            "text": "",
+            "tags": []
+        }
+
+        notes = self.getNotes()
+        notes.append(note)
+
+        self.saveFile(notes)
 
     def getNotes(self):
-        pass
+        with open(path, "r", encoding="utf-8") as file:
+            return json.load(file)
 
-    def updateNoteText(self):
-        pass
+    def updateNoteText(self, title, newText):
 
-    def deleteNote(self):
-        pass
+        notes = self.getNotes()
 
-    def addTag(self):
-        pass
+        for note in notes:
+            if note["title"] == title:
+                note["text"] = newText
+
+        self.saveFile(notes)
+        
+
+    def deleteNote(self, deleteTitle):
+        
+        notes = self.getNotes()
+
+        for note in notes:
+            if note["title"] == deleteTitle:
+                notes.remove(note)
+
+        self.saveFile(notes)
+
+
+    def addTag(self, title, tag):
+        notes = self.getNotes()
+
+        for note in notes:
+            if note["title"] == title:
+                note["tags"].append(tag)
 
     def deleteTag(self):
         pass
+
+    def saveFile(self, notes):
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump(notes, file)
 
