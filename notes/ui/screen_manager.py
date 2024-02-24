5ad97df54aa1407ed78f7fc2c8b5ba2e910e2677
markgardie <60879509+markgardie@users.noreply.github.com>
2024-02-24 13:31:30 +0200

    notes, lesson 4

--- notes/ui/screen_manager.py
+++ notes/ui/screen_manager.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Qt
 from main_screen import MainScreen
 from data.notes_dao import NotesDao
+from PyQt5.QtWidgets import QWidget, QInputDialog
 
 class ScreenManager():
 
@@ -11,10 +12,30 @@ class ScreenManager():
     def setListeners(self):
         pass
 
-    def createNote(self, title):
-        pass
+    def createNote(self):
+        
+        noteTitle, ok = QInputDialog.getText(
+            self.mainScreen, 
+            "Додатки нотатку", 
+            "Введіть назву нотатки"
+        )
 
-    def showNotes(self):
+        if noteTitle != "" and ok:
+            self.notesDao.createNote(noteTitle)
+
+    def showNotesList(self):
+
+        notesList = self.notesDao.getNotes()
+        titleList = []
+
+        for note in notesList:
+            titleList.append(note["title"])
+
+        for title in titleList:
+            self.mainScreen.notesListWidget.addItem(title)
+
+
+    def showNoteText(self):
         pass
 
     def updateNoteText(self, title, newText):
