cacd21d8c150af9054308f2241c8ead4a73345c5
markgardie <60879509+markgardie@users.noreply.github.com>
2024-03-02 11:32:32 +0200

    final, lesson 2

--- notepad/main.py
+++ notepad/main.py
@@ -17,11 +17,11 @@ from PyQt5.QtWidgets import (
     QWidget,
 )
 
+
 class MainScreen(QMainWindow):
 
     def __init__(self):
         super().__init__()
-
     
     def file_open(self):
         path, _ = QFileDialog.getOpenFileName(
@@ -43,3 +43,43 @@ class MainScreen(QMainWindow):
                 self.path = path
                 self.editor.setPlainText(text)
                 self.update_title()
+
+    def file_save(self):
+
+        if self.path is None:
+            # If we do not have a path, we need to use Save As.
+            return self.file_saveas()
+
+        self._save_to_path(self.path)
+
+    def file_saveas(self):
+        path, _ = QFileDialog.getSaveFileName(
+            self,
+            "Save file",
+            "",
+            "Text documents (*.txt);;All files (*.*)",
+        )
+
+        if not path:
+            # If dialog is cancelled, will return ''
+            return
+
+        self._save_to_path(path)
+
+    def _save_to_path(self, path):
+        text = self.editor.toPlainText()
+        try:
+            with open(path, "w") as f:
+                f.write(text)
+
+        except Exception as e:
+            self.dialog_critical(str(e))
+
+        else:
+            self.path = path
+            self.update_title()
+
+    def file_print(self):
+        dlg = QPrintDialog()
+        if dlg.exec_():
+            self.editor.print_(dlg.printer())
