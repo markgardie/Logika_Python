0af30c7af2107f0c617f9eae0268446b311937ce
markgardie <60879509+markgardie@users.noreply.github.com>
2024-03-31 11:32:23 +0300

    initial commit

--- files_intro/quotes.py
+++
@@ -1,9 +0,0 @@
-
-path = r"D:\Mark\Desktop\Logika_Python\files_intro\quotes.txt"
-
-with open(path, "r", encoding="utf-8") as file:
-    poem = file.read()
-    print(poem)
-
-with open(path, "a", encoding="utf-8") as file:
-    file.write("Тарас Шевченко")
