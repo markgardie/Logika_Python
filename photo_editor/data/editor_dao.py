6ceb3c64fb7363c23d06a7d1fc8ec40444b6cf08
markgardie <60879509+markgardie@users.noreply.github.com>
2024-04-13 14:58:37 +0300

    photo_editor, lesson 2

---
+++ photo_editor/data/editor_dao.py
@@ -0,0 +1,15 @@
+import os
+from PIL import Image
+
+
+class EditorDao():
+
+    def __init__(self):
+        self.extensions = ['.jpg','.jpeg', '.png', '.gif', '.bmp']
+
+    def open(self, name):
+        return os.path.join(self.dirPath, name)
+    
+
+
+    
