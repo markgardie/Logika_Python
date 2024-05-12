b1f2cfadac92d01749cc45158a2652572618a928
markgardie <60879509+markgardie@users.noreply.github.com>
2024-05-12 15:57:19 +0300

    ruffier, lesson 4

--- ruffier/ui/main_controller.py
+++ ruffier/ui/main_controller.py
@@ -1,21 +1,30 @@
 from domain.ruffier_use_case import*
 from domain.user import User
 
+
 class MainController():
 
     def __init__(self):
-        pass
+        self.intro_screen = IntroScreen()
+
 
     def click_listeners(self):
-        pass
+        self.intro_screen.next_button.on_press = self.navigate_to_second
 
     
     def create_user(self):
-        pass
+        self.user = User(
+            self.intro_screen.name_input.text,
+            self.intro_screen.age_input.text
+        )
     
     def save_p1(self):
-       pass
+        self.p1 = self.first_pulse_screen.pulse_input.text
     
 
     def finish(self):
-        pass
+        index = ruffier_index(
+            self.p1,
+            self.rest_pulse_screen.pulse_input2.text,
+            self.rest_pulse_screen.pulse_input3.text
+        )
