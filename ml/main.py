5124f8526f2eb3f6b58a24945df0ec7d7aa13096
markgardie <60879509+markgardie@users.noreply.github.com>
2024-05-16 16:51:46 +0300

    ml, lesson 3

--- ml/main.py
+++ ml/main.py
@@ -38,3 +38,18 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
 
+X = df.drop("Survived", axis = 1) 
+y = df["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = KNeighborsClassifier(n_neighbors=3)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred) * 100)
