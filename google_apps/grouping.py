a4929ed407613dc0d3f233cafb30fa1097738dca
markgardie <60879509+markgardie@users.noreply.github.com>
2024-06-23 15:44:58 +0300

    data analysis, lesson 4

--- google_apps/grouping.py
+++ google_apps/grouping.py
@@ -24,13 +24,16 @@ print(round(rating_mean["Paid"], 2))
 
 # 3.2 На скільки середній рейтинг ('Rating') безкоштовних ('Free') додатків менший за середній рейтинг платних ('Paid')?
 # Відповідь запиши з точністю до сотих.
-
+print(round(rating_mean["Free"] - rating_mean["Paid"], 2))
 
 
 # 4 Чому дорівнює мінімальний та максимальний розмір ('Size') додатків у категорії ('Category') 'COMICS'?
 # Запиши відповіді з точністю до сотих.
-
-
+temp = df.groupby(by = "Category")
+size_min = temp["Size"].min()
+size_max = temp["Size"].max()
+print(round(size_min["COMICS"], 2))
+print(round(size_max["COMICS"], 2))
 
 # Бонус 1. Скільки додатків з рейтингом ('Rating') більше 4.5 у категорії ('Category') 'FINANCE'?
 filt = df[df["Rating"] > 4.5]
