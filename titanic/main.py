889ce3639803a03b6bd9a27c957bbe2f136fe612
markgardie <60879509+markgardie@users.noreply.github.com>
2024-06-30 14:22:45 +0300

    ml, lesson 1

---
+++ titanic/main.py
@@ -0,0 +1,5 @@
+# Крок 2. Створення моделі
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
