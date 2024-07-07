7831c9bb522f516186682b4fb41a3289cd02785d
markgardie <60879509+markgardie@users.noreply.github.com>
2024-07-07 10:31:00 +0300

    final, lesson 3

--- say_hello/views.py
+++ say_hello/views.py
@@ -5,4 +5,17 @@ from say_hello.models import Message
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
-    pass
+    form = HelloForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        body = form.body.data
+        new_message = Message(name = name, body = body)
+        db.session.add(new_message)
+        db.session.commit()
+        flash("Повідомлення додано успішно")
+        return redirect(url_for("index"))
+    
+    messages = Message.query.order_by(Message.timestamp.desc()).all()
+    return render_template("index.html", form = form, messages = messages)
+
+
