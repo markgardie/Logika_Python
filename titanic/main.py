b1bb9056da0d8ed900ce11573214b62489db90c5
markgardie <60879509+markgardie@users.noreply.github.com>
2024-07-07 12:02:18 +0300

    ml, lesson 2

--- titanic/main.py
+++ titanic/main.py
@@ -3,3 +3,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
+
+y = df["Survived"]
+X = df.drop(["PassengerId", "Name", "Ticket", "Fare", "Cabin"], axis = 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
