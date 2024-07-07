d4a91500bdcdcc552994aa882c690d7f387faa5c
markgardie <60879509+markgardie@users.noreply.github.com>
2024-07-07 13:46:36 +0300

    ml, lesson 3

--- titanic/main.py
+++ titanic/main.py
@@ -9,3 +9,15 @@ X = df.drop(["PassengerId", "Name", "Ticket", "Fare", "Cabin"], axis = 1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+classifier = KNeighborsClassifier()
+
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred) * 100
+
+print("Точність:", accuracy)
