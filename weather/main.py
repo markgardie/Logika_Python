259346d3ff4fba8b2cc3ca835bec74222bdd3b35
markgardie <60879509+markgardie@users.noreply.github.com>
2024-07-15 13:21:52 +0300

    final, lesson 5

--- weather/main.py
+++ weather/main.py
@@ -25,3 +25,49 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         self.thread_pool = QThreadPool()
 
         self.show()
+
+    def update_weather(self):
+        worker = WeatherWorker(self.lineEdit.text())
+        worker.signals.error.connect(self.alert)
+        worker.signals.result.connect(self.weather_result)
+        self.thread_pool.start(worker)
+
+    def alert(self, message):
+        QMessageBox.warning(self, "Помилка", message)
+
+    def weather_result(self, weather, forecasts):
+            self.latitudeLabel.setText("%.2f °" % weather["coord"]["lat"])
+            self.longitudeLabel.setText("%.2f °" % weather["coord"]["lon"])
+
+            self.windLabel.setText("%.2f m/s" % weather["wind"]["speed"])
+
+            self.temperatureLabel.setText("%.1f °C" % weather["main"]["temp"])
+            self.pressureLabel.setText("%d" % weather["main"]["pressure"])
+            self.humidityLabel.setText("%d" % weather["main"]["humidity"])
+
+            self.sunriseLabel.setText(from_ts_to_time_of_day(weather["sys"]["sunrise"]))
+
+            self.weatherLabel.setText(
+                "%s (%s)"
+                % (
+                    weather["weather"][0]["main"],
+                    weather["weather"][0]["description"],
+                )
+            )
+
+            self.set_weather_icon(self.weatherIcon, weather["weather"])
+
+            for n, forecast in enumerate(forecasts["list"][:5], 1):
+                getattr(self, "forecastTime%d" % n).setText(from_ts_to_time_of_day(forecast["dt"]))
+                self.set_weather_icon(getattr(self, "forecastIcon%d" % n), forecast["weather"])
+                getattr(self, "forecastTemp%d" % n).setText("%.1f °C" % forecast["main"]["temp"])
+
+    def set_weather_icon(self, label, weather):
+        pixmap = QPixmap(os.path.join("images", "%s.png" % weather[0]["icon"]))
+        label.setPixmap(pixmap)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    app.exec()
