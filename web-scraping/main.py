d9a78fd6ff074a71c8a1cd299f17df3c0f11d2a9
markgardie <60879509+markgardie@users.noreply.github.com>
2024-08-06 13:36:43 +0300

    web-scraping, lesson 1

---
+++ web-scraping/main.py
@@ -0,0 +1,11 @@
+import requests 
+from bs4 import BeautifulSoup 
+
+url = 'https://quotes.toscrape.com/'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('span', class_ = 'text')
+
+for i in range(quotes):
+    print(quotes[i])
