b65b96bf9c097eac556c0eab4c25cec247f0abf4
markgardie <60879509+markgardie@users.noreply.github.com>
2024-08-14 17:22:10 +0300

    scraping, lesson 2

--- web-scraping/main.py
+++ web-scraping/main.py
@@ -1,11 +1,47 @@
 import requests 
 from bs4 import BeautifulSoup 
+import sqlite3
 
 url = 'https://quotes.toscrape.com/'
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 quotes = soup.find_all('span', class_ = 'text')
+authors = soup.find_all('small', class_ = 'text')
+tags = soup.find_all('div', class_ = 'text')
 
 for i in range(quotes):
     print(quotes[i])
+    print(authors[i])
+
+for tag in tags:
+    print(tag.text.split()[1:])
+
+
+conn = sqlite3.connect("web-scraping\data_from_web.db")
+cursor = conn.cursor()
+
+createTable = '''
+    CREATE TABLE IF NOT EXISTS quotes (
+        author TEXT
+        quote_text TEXT
+        tags TEXT
+    )
+'''
+
+cursor.execute(createTable)
+
+insert = '''
+    INSERT INTO quotes (author, quote_text, tags)
+    VALUES (?, ?, ?)
+'''
+
+for i in range(len(quotes)):
+    author = authors[i].text
+    quote = quotes[i].text
+    tag = ", ".join(tags[i].text.split()[1:])
+
+    cursor.execute(insert, [author, quote, tag])
+    conn.commit()
+
+        
