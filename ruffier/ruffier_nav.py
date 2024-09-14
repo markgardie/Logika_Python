a4a92037710a0146f813610e22a44f9229ed4c9d
markgardie <60879509+markgardie@users.noreply.github.com>
2024-09-14 13:29:16 +0300

    ruffier, lesson 2

---
+++ ruffier/ruffier_nav.py
@@ -0,0 +1,27 @@
+from kivy.uix.screenmanager import ScreenManager
+from instr_screen import InstrScreen
+from pulse_screen1 import PulseScreen1
+from squat_screen import SquatScreen
+from pulse_screen2 import PulseScreen2
+from result_screen import ResultScreen
+
+class RuffierNavigation(ScreenManager):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.create_screens()
+        self.add_screens()
+        self.click_listeners()
+
+    def create_screens(self):
+        self.instr_screen = InstrScreen(name = "instr_screen")
+
+    def add_screens(self):
+        self.add_widget(self.instr_screen)
+
+    def click_listeners(self):
+        self.instr_screen.btn.on_press = self.navigate_to_p1
+
+    def navigate_to_p1(self):
+        self.current = "pulse_screen1"
+
---
+++ ruffier/user.py
@@ -0,0 +1,8 @@
+
+class User():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.index = None
+        self.level = None
