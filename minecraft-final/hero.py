8623e70a15f11ac2a1cf40eb3a664acefe61d406
markgardie <60879509+markgardie@users.noreply.github.com>
2024-10-03 16:18:16 +0300

    minecraft, pre-final

---
+++ minecraft-final/hero.py
@@ -0,0 +1,242 @@
+from direct.showbase.ShowBase import ShowBase
+
+KEY_SWITCH_CAMERA = 'c' # камера прив'язана до героя чи ні
+KEY_SWITCH_MODE = 'z' # можна проходити крізь перешкоди чи ні
+
+
+KEY_FORWARD = 'w'   # крок вперед (куди дивиться камера)
+KEY_BACK = 's'      # крок назад
+KEY_LEFT = 'a'      # крок вліво (вбік від камери)
+KEY_RIGHT = 'd'     # крок вправо
+KEY_UP = 'e'      # крок вгору
+KEY_DOWN = 'q'     #крок вниз
+
+
+KEY_TURN_LEFT = 'n'     # поворот камери праворуч (а світу - ліворуч)
+KEY_TURN_RIGHT = 'm'    # поворот камери ліворуч (а світу – праворуч)
+
+KEY_BUILD = 'b'     # побудувати блок перед собою
+KEY_DESTROY = 'v'
+
+HERO_COLOR = (1, 0.5, 0)
+SCALE = 0.3
+HEADING = 180
+
+CAMERA_POS = (0, 0, 1.5)
+
+KEY_SAVEMAP = 'k'
+KEY_LOADMAP = 'l'
+
+class Hero():
+   def __init__(self, pos, land):
+       self.land = land
+       self.mode = True # режим проходження крізь усе
+       self.hero = loader.loadModel('smiley')
+       self.hero.setColor(1, 0.5, 0)
+       self.hero.setScale(0.3)
+       self.hero.setH(180)
+       self.hero.setPos(pos)
+       self.hero.reparentTo(render)
+       self.cameraBind()
+       self.accept_events()
+
+
+   def cameraBind(self):
+       base.disableMouse()
+       # base.camera.setH(180)
+       base.camera.reparentTo(self.hero)
+       base.camera.setPos(0, 0, 1.5)
+       self.cameraOn = True
+
+
+   def cameraUp(self):
+       pos = self.hero.getPos()
+       base.mouseInterfaceNode.setPos(-pos[0], -pos[1], -pos[2]-3)
+       base.camera.reparentTo(render)
+       base.enableMouse()
+       self.cameraOn = False
+
+
+   def changeView(self):
+       if self.cameraOn:
+           self.cameraUp()
+       else:
+           self.cameraBind()
+
+
+   def turn_left(self):
+       self.hero.setH((self.hero.getH() + 5) % 360)
+
+
+   def turn_right(self):
+       self.hero.setH((self.hero.getH() - 5) % 360)
+
+
+   def look_at(self, angle):
+       ''' повертає координати, в які переміститься персонаж, що стоїть у точці (x, y),
+        якщо він робить крок у напрямку angle'''
+
+
+       x_from = round(self.hero.getX())
+       y_from = round(self.hero.getY())
+       z_from = round(self.hero.getZ())
+
+
+       dx, dy = self.check_dir(angle)
+       x_to = x_from + dx
+       y_to = y_from + dy
+       return x_to, y_to, z_from
+
+
+   def just_move(self, angle):
+       '''переміщається у потрібні координати у будь-якому випадку'''
+       pos = self.look_at(angle)
+       self.hero.setPos(pos)
+
+
+   def move_to(self, angle):
+       if self.mode:
+           self.just_move(angle)
+       else:
+           self.try_move(angle)
+  
+   def check_dir(self,angle):
+       ''' повертає заокруглені зміни координат X, Y,
+       відповідні переміщенню у бік кута angle.
+       Координата Y зменшується, якщо персонаж дивиться на кут 0,
+       та збільшується, якщо дивиться на кут 180.
+       Координата X збільшується, якщо персонаж дивиться на кут 90,
+       та зменшується, якщо дивиться на кут 270.
+           кут 0 (від 0 до 20) -> Y - 1
+           кут 45 (від 25 до 65) -> X + 1, Y - 1
+           кут 90 (від 70 до 110) -> X + 1
+           від 115 до 155 -> X + 1, Y + 1
+           від 160 до 200 -> Y + 1
+           від 205 до 245 -> X - 1, Y + 1
+           від 250 до 290 -> X - 1
+           від 290 до 335 -> X - 1, Y - 1
+           від 340 -> Y - 1
+ '''
+       if angle >= 0 and angle <= 20:
+           return (0, -1)
+       elif angle <= 65:
+           return (1, -1)
+       elif angle <= 110:
+           return (1, 0)
+       elif angle <= 155:
+           return (1, 1)
+       elif angle <= 200:
+           return (0, 1)
+       elif angle <= 245:
+           return (-1, 1)
+       elif angle <= 290:
+           return (-1, 0)
+       elif angle <= 335:
+           return (-1, -1)
+       else:
+           return (0, -1)
+
+
+   def forward(self):
+       angle =(self.hero.getH()) % 360
+       self.move_to(angle)
+
+
+   def back(self):
+       angle = (self.hero.getH()+180) % 360
+       self.move_to(angle)
+  
+   def left(self):
+       angle = (self.hero.getH() + 90) % 360
+       self.move_to(angle)
+
+
+   def right(self):
+       angle = (self.hero.getH() + 270) % 360
+       self.move_to(angle)
+
+
+   def changeMode(self):
+       if self.mode:
+           self.mode = False
+       else:
+           self.mode = True
+  
+   def try_move(self, angle):
+       '''переміщається, якщо може'''
+       pos = self.look_at(angle)
+       if self.land.isEmpty(pos):
+           # маємо вільно. Можливо, треба впасти вниз:
+           pos = self.land.findHighestEmpty(pos)
+           self.hero.setPos(pos)
+       else:
+           # маємо зайнято. Якщо вийде, заберемося на цей блок:
+           pos = pos[0], pos[1], pos[2] + 1
+           if self.land.isEmpty(pos):
+               self.hero.setPos(pos)
+               # не вийде забратися - стоїмо на місці
+   def up(self):
+       if self.mode:
+           self.hero.setZ(self.hero.getZ() + 1)
+
+
+   def down(self):
+       if self.mode and self.hero.getZ() > 1:
+           self.hero.setZ(self.hero.getZ() - 1)
+  
+   def build(self):
+       angle = self.hero.getH() % 360
+       pos = self.look_at(angle)
+       if self.mode:
+           self.land.addBlock(pos)
+       else:
+           self.land.buildBlock(pos)
+
+
+   def destroy(self):
+       angle = self.hero.getH() % 360
+       pos = self.look_at(angle)
+       if self.mode:
+           self.land.delBlock(pos)
+       else:
+           self.land.delBlockFrom(pos)
+
+
+   def accept_events(self):
+       base.accept(KEY_TURN_LEFT, self.turn_left)
+       base.accept(KEY_TURN_LEFT + '-repeat', self.turn_left)
+       base.accept(KEY_TURN_RIGHT, self.turn_right)
+       base.accept(KEY_TURN_RIGHT + '-repeat', self.turn_right)
+
+
+       base.accept(KEY_FORWARD, self.forward)
+       base.accept(KEY_FORWARD + '-repeat', self.forward)
+       base.accept(KEY_BACK, self.back)
+       base.accept(KEY_BACK + '-repeat', self.back)
+       base.accept(KEY_LEFT, self.left)
+       base.accept(KEY_LEFT + '-repeat', self.left)
+       base.accept(KEY_RIGHT, self.right)
+       base.accept(KEY_RIGHT + '-repeat', self.right)
+
+
+       base.accept(KEY_SWITCH_CAMERA, self.changeView)
+
+
+       base.accept(KEY_SWITCH_MODE, self.changeMode)
+
+
+       base.accept(KEY_UP, self.up)
+       base.accept(KEY_UP + '-repeat', self.up)
+       base.accept(KEY_DOWN, self.down)
+       base.accept(KEY_DOWN + '-repeat', self.down)
+
+
+       base.accept(KEY_BUILD, self.build)
+       base.accept(KEY_DESTROY, self.destroy)
+
+    
+       base.accept(KEY_SAVEMAP, self.land.saveMap)
+       base.accept(KEY_LOADMAP, self.land.loadMap)
+
+
+
---
+++ minecraft-final/mapmanager.py
@@ -0,0 +1,145 @@
+from direct.showbase.ShowBase import ShowBase
+import pickle
+
+class MapManager():
+    def __init__(self):
+       self.model = 'block.egg' # модель кубика лежить у файлі block.egg
+       # # використовуються такі текстури:
+       self.texture = 'block.png'        
+       self.colors = [
+           (0.2, 0.2, 0.35, 1),
+           (0.2, 0.5, 0.2, 1),
+           (0.7, 0.2, 0.2, 1),
+           (0.5, 0.3, 0.0, 1)
+       ] #rgba
+
+       # створюємо основний вузол картки:
+       self.startNew()
+        # створюємо будівельні блоки   
+      #  self.addBlock((0,10, 0))
+
+    def startNew(self):
+       """створює основу для нової картки"""
+       self.land = render.attachNewNode("Land") # вузол, до якого прив'язані всі блоки картки
+  
+    def getColor(self, z):
+       if z < len(self.colors):
+           return self.colors[z]
+       else:
+           return self.colors[len(self.colors) - 1]
+
+    
+    def addBlock(self, position):
+       self.block = loader.loadModel(self.model)
+       self.block.setTexture(loader.loadTexture(self.texture))
+       self.block.setPos(position)
+       self.color = self.getColor(int(position[2]))
+       self.block.setColor(self.color)
+       self.block.reparentTo(self.land)
+
+    def loadLand(self, filename):
+       """створює карту землі з текстового файлу, повертає її розміри"""
+       self.clear()
+       with open(filename) as file:
+           y = 0
+           for line in file:
+               x = 0
+               line = line.split(' ')
+               for z in line:
+                   for z0 in range(int(z)+1):
+                       block = self.addBlock((x, y, z0))
+                   x += 1
+               y += 1
+
+       return x,y
+
+    def clear(self):
+       """обнуляє карту"""
+       self.land.removeNode()
+       self.startNew()
+
+    def findBlocks(self, pos):
+       return self.land.findAllMatches("=at=" + str(pos))
+
+
+    def isEmpty(self, pos):
+       blocks = self.findBlocks(pos)
+       if blocks:
+           return False
+       else:
+           return True
+
+
+    def findHighestEmpty(self, pos):
+       x, y, z = pos
+       z = 1
+       while not self.isEmpty((x, y, z)):
+           z += 1
+       return (x, y, z)
+
+
+    def buildBlock(self, pos):
+       """Ставимо блок з урахуванням гравітації: """
+       x, y, z = pos
+       new = self.findHighestEmpty(pos)
+       if new[2] <= z + 1:
+           self.addBlock(new)
+
+
+    def delBlock(self, position):
+       """видаляє блоки у зазначеній позиції """
+       blocks = self.findBlocks(position)
+       for block in blocks:
+           block.removeNode()
+
+
+    def delBlockFrom(self, position):
+       x, y, z = self.findHighestEmpty(position)
+       pos = x, y, z - 1
+       for block in self.findBlocks(pos):
+               block.removeNode()
+
+    def saveMap(self):
+       """зберігає всі блоки, включаючи споруди, у бінарний файл"""
+
+
+       """повертає колекцію NodePath для всіх існуючих у карті світу блоків"""
+       blocks = self.land.getChildren()
+       # відкриваємо бінарний файл на запис
+       with open('my_map.dat', 'wb') as fout:
+
+
+           # зберігаємо на початок файлу кількість блоків
+           pickle.dump(len(blocks), fout)
+
+
+           # обходимо всі блоки
+           for block in blocks:
+               # зберігаємо позицію
+               x, y, z = block.getPos()
+               pos = (int(x), int(y), int(z))
+               pickle.dump(pos, fout)
+
+
+    def loadMap(self):
+       # видаляємо всі блоки
+       self.clear()
+
+
+       # відкриваємо бінарний файл на читання
+       with open('my_map.dat', 'rb') as fin:
+          
+           # зчитуємо кількість блоків
+           length = pickle.load(fin)
+
+
+           for i in range(length):
+               # зчитуємо позицію
+               pos = pickle.load(fin)
+
+
+               # створюємо новий блок
+               self.addBlock(pos)
+
+
+
