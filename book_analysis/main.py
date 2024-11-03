05b5f7868168f2cfe167b8722e5a5415b1e88ded
markgardie <60879509+markgardie@users.noreply.github.com>
2024-11-03 12:02:08 +0200

    book analysis, lesson 1

---
+++ book_analysis/main.py
@@ -0,0 +1,9 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import nltk
+import matplotlib.pyplot as plt
+from scipy.fftpack import dct, idct
+import io
+import os
+import json
+import numpy as np
+
