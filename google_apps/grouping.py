6b65c9077be3d0e8f068e51673d224a1465ea93d
markgardie <60879509+markgardie@users.noreply.github.com>
2024-11-11 08:57:31 +0200

    pandas, lesson 4

--- google_apps/grouping.py
+++ google_apps/grouping.py
@@ -1,29 +1,51 @@
 import pandas as pd
-df = pd.read_csv(r"google_apps\grouping.py")
+df = pd.read_csv(r"google_apps\GoogleApps.csv")
 
 
 # 1 Скільки всього програм з категорією ('Category') 'BUSINESS'?
 business_count = df["Category"].value_counts()
 print(business_count["BUSINESS"])
 
-# 1.1 Скільки платних додатків (група Paid, розбивка по колонці Type)
-business_count = df["Type"].value_counts()
-print(business_count["Paid"])
-
 # 1.2 Скільки додатків створено для підлітків (Content Rating, Teen)
 
 
 # 2 Чому дорівнює співвідношення кількості додатків для підлітків ('Teen') і для дітей старше 10 ('Everyone 10+')?
-# Відповідь запиши з точністю до сотих.
+# Відповідь запиши з точністю до сотих. 
+temp = df["Content Rating"].value_counts()
+temp = temp["Teen"] / temp["Everyone 10+"]
+print(temp)
 
+# 2.1 На скільки кількість безкоштовних додатків більша за кількість платних додатків. 
+temp = df["Type"].value_counts()
+temp = temp["Free"] - temp["Paid"]
+print(temp)
 
 
 # 3.1 Чому дорівнює середній рейтинг ('Rating') платних ('Paid') додатків?
 # Відповідь запиши з точністю до сотих.
+temp = df.groupby(by = "Type")
+temp = temp["Rating"].mean()
+print(round(temp["Paid"], 2))
 
-
-# 3.2 На скільки середній рейтинг ('Rating') безкоштовних ('Free') додатків менший за середній рейтинг платних ('Paid')?
+# 3.2 Чому дорівнює середня ціна додатків в категорії BUSINESS
 # Відповідь запиши з точністю до сотих.
+temp = df.groupby(by = "Category")
+temp = temp["Price"].mean()
+print(round(temp["BUSINESS"], 2))
+
+# 3.3 Чому дорівнює мінімальна ціна в категорії EDUCATION?
+temp = df.groupby(by = "Category")
+temp = temp["Price"].mean()
+print(round(temp["BUSINESS"], 2))
+
+# dz 1.1. Скільки додатків в категорії SOCIAL
+
+# dz 1.2 Скільки додатків в групі Everyone
+
+# dz 2.1 Чому дорівнює максимальний рейтинг в групі Teen?
+
+# dz 2.2 Чому дорівнює середня кількість установок в категорії MEDICAL
+
 
 
 # 4 Чому дорівнює мінімальний та максимальний розмір ('Size') додатків у категорії ('Category') 'COMICS'?
