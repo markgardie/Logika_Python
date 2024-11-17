493feff213e0877639c5b2f729256e23c843a22e
markgardie <60879509+markgardie@users.noreply.github.com>
2024-11-17 15:38:18 +0200

    quiz, lesson 3

--- quiz/controllers/main_contoller.py
+++ quiz/controllers/main_contoller.py
@@ -17,7 +17,21 @@ class MainController():
             return redirect(url_for("quiz"))
         
     def quiz(self):
-        pass
+        if not ('question_id' in session) or int(session["question_id"]) < 0:
+            return redirect(url_for("index"))
+        else:
+            question = self.dao.get_question(session["question_id"])
+
+            if request.method == "POST":
+                answer = request.form.get("ans_text")
+                if answer == question[2]:
+                    session["result"] = "Відповідь правильна"
+                else:
+                    session["result"] = "Відповідь неправильна"
+            else:
+                answers = list(question[2:])
+                shuffle(answers)
+                return render_template("quiz.html", question = question, answer_list = answers)
         
     def result(self):
         return render_template("result.html", result = session["result"])
