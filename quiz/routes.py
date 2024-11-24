31fdeaec82e5b0a9a4f4da2a1c1a03abbcd8fa2f
markgardie <60879509+markgardie@users.noreply.github.com>
2024-11-24 16:09:46 +0200

    quiz, lesson 4

--- quiz/routes.py
+++ quiz/routes.py
@@ -4,5 +4,6 @@ main_controller = MainController()
 
 def setup_routes(app):
     app.add_url_rule("/", "index", main_controller.index, methods = ["post", "get"])
-    
+    app.add_url_rule("/quiz", "quiz", main_controller.quiz, methods = ["post", "get"])
+    app.add_url_rule("/result", "result", main_controller.result, methods = ["get"])
     
