a619b0a8a15f9c1ff2725473d877e49db0968490
markgardie <60879509+markgardie@users.noreply.github.com>
2024-11-25 11:41:54 +0200

    book analysis, lesson 1

--- book_analysis/main.py
+++ book_analysis/main.py
@@ -7,3 +7,36 @@ import os
 import json
 import numpy as np
 
+
+def analyze_one_book():
+
+    book_path = r"book_analysis\matrix.txt"
+    with open(book_path, encoding="utf-8") as book_file:
+        book_text = book_file.read()
+
+    sentences = nltk.sent_tokenize(book_text)
+    scores = []
+    analyzer = SentimentIntensityAnalyzer()
+
+    for sent in sentences:
+        score = analyzer.polarity_scores(sent)["compound"]
+        scores.append(score)
+
+
+    scores_dct = dct(scores, norm='ortho', type=2)
+    scores_lpf = scores_dct.copy()
+       
+    scores_lpf[10:] = 0
+
+
+    filtered_scores = idct(scores_lpf, norm='ortho')
+
+    normalized_narrative = 100
+
+    filtered_scores = np.interp(np.linspace(0, 1, normalized_narrative), np.linspace(0, 1, len(filtered_scores)), filtered_scores)
+
+    plt.plot(filtered_scores)
+    plt.title("")
+    plt.show()
+
+analyze_one_book()
