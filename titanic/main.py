e03a8c8d6b7a317f311006691cc94f9f71cecdad
markgardie <60879509+markgardie@users.noreply.github.com>
2024-12-16 13:08:02 +0200

    ml, lesson 2

--- titanic/main.py
+++ titanic/main.py
@@ -12,3 +12,33 @@ df['Embarked'].fillna("S", axis = 1)
 
 df.drop(["Embarked"], axis = 1)
 
+def fill_gender(gender):
+    if gender == "male":
+        return 1
+    return 0
+
+df["Sex"] = df.apply(fill_gender)
+
+age1 = df[df["Pclass" == 1]]["Age"].median()
+# age2
+# age3
+
+def fill_age(row):
+    if pd.isnull(row["Age"]):
+        if row["Pclass"] == 1:
+            return age1
+        # Pclass == 2 
+        # Pclass == 3
+    return row["Age"]
+
+df["Age"] = df.apply(fill_age)
+
+
+y = df["Survived"]
+X = df.drop(["PassengerId", "Name", "Ticket", "Fare", "Cabin"], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
