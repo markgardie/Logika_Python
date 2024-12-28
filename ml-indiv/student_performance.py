3ba4784260637d1755db3263aed025135798ebfd
markgardie <60879509+markgardie@users.noreply.github.com>
2024-12-28 15:04:57 +0200

    ml, ind, lesson 2

--- ml-indiv/student_performance.py
+++ ml-indiv/student_performance.py
@@ -3,6 +3,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Кореляції
+
 df = pd.read_csv('StudentPerformanceFactors.csv')
 
 df_numeric = df.select_dtypes(include=['number'])
@@ -11,3 +13,16 @@ corr_matrix = df_numeric.corr()[['Exam Score']]
 sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', linewidths=0.5, vmin=1, vmax=1)
 plt.title("Correlation")
 plt.show()
+
+# Навчання моделі
+
+# X, y
+# train test split
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_test)
+accuracy_r2 = r2_score(y_test, y_pred)
+
+print(accuracy_r2)
