b4b31df8ac7bf0028c7f3f44af84b1bbb82c89d3
markgardie <60879509+markgardie@users.noreply.github.com>
2024-12-30 12:11:24 +0200

    ml projects, lesson 1

---
+++ ml-projects/student_performance.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Correlation
+df = pd.read_csv('/')
+
+df_numeric = df.select_dtypes(include=['number'])
+corr = df_numeric.corr()[["Exam Score"]]
+
+sns.heatmap(corr, annot=True, cmap="coolwarm", linewidths=0.5, vmin=-1, vmax=1)
+plt.title("Correlation")
+plt.show()
+
+# Regression
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
+# x, y
+# train test split
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_test)
+r2_accuracy = r2_score(y_test, y_pred)
+print(r2_accuracy)
+
+# Regression plot
+plt.figure(figsize=(10, 6))
+plt.scatter(y_test, y_pred, color="blue", alpha=0.5, label = "Predicted vs Actual")
+
