00fe25d148755991f41ada7b51d7d294d9a86d19
markgardie <60879509+markgardie@users.noreply.github.com>
2025-02-15 11:36:23 +0200

    minecraft, lesson 3

--- minecraft/hero.py
+++ minecraft/hero.py
@@ -28,19 +28,38 @@ KEY_LOADMAP = 'l'
 
 class Hero():
 
-    def __init__(self):
-        pass
+    def __init__(self, land, pos):
+        self.land = land
+        self.isFreeMode = False  
+        self.heroModel = loader.loadModel('smiley')
+        self.heroModel.setColor(1, 0.5, 0)
+        self.heroModel.setScale(SCALE)
+        self.heroModel.setH(HEADING)
+        self.heroModel.setPos(pos)
+        self.heroModel.reparentTo(render)
+        self.cameraBind()
+        self.accept_events()
 
 
     def cameraBind(self):
         base.disableMouse()
         base.camera.setH(180)
-        base.camera.reparantTo(self.heroModel)
+        base.camera.reparentTo(self.heroModel)
         base.camera.setPos(CAMERA_POS)
         self.cameraBinded = True
 
     def cameraUnbind(self):
-        pass
+        pos = self.heroModel.getPos()
+        base.mouserInterfaceNode.setPos(-pos[0], -pos[1], -pos[2] - 3)    
+        base.enableMouse()
+        base.camera.reparentTo(render)
+        self.cameraBinded = False
+
+    def changeView(self):
+        if self.cameraBinded:
+            self.cameraUnbind()
+        else:
+            self.cameraBind()
     
     def move_to(self, angle):
         if self.isFreeMode:
@@ -61,6 +80,66 @@ class Hero():
             pos = pos[0], pos[1], pos[2] + 1
             if self.land.isEmpty(pos):
                 self.heroModel.setPos(pos)
+
+    def look_at(self, angle):
+        x_from = round(self.heroModel.getX())
+        y_from = round(self.heroModel.getY())
+        z_from = round(self.heroModel.getZ())
+
+        dx, dy = self.check_dirs()
+
+        x_to = x_from + dx
+        y_to = y_from + dy
+
+        return x_to, y_to, z_from
+
+
+    def check_dirs(self,angle):
+        ''' повертає заокруглені зміни координат X, Y,
+        відповідні переміщенню у бік кута angle.
+        Координата Y зменшується, якщо персонаж дивиться на кут 0,
+        та збільшується, якщо дивиться на кут 180.
+        Координата X збільшується, якщо персонаж дивиться на кут 90,
+        та зменшується, якщо дивиться на кут 270.
+            кут 0 (від 0 до 20) -> Y - 1
+            кут 45 (від 25 до 65) -> X + 1, Y - 1
+            кут 90 (від 70 до 110) -> X + 1
+            від 115 до 155 -> X + 1, Y + 1
+            від 160 до 200 -> Y + 1
+            від 205 до 245 -> X - 1, Y + 1
+            від 250 до 290 -> X - 1
+            від 290 до 335 -> X - 1, Y - 1
+            від 340 -> Y - 1
+    '''
+        if angle >= 0 and angle <= 20:
+            return (0, -1)
+        elif angle <= 65:
+            return (1, -1)
+        elif angle <= 110:
+            return (1, 0)
+        elif angle <= 155:
+            return (1, 1)
+        elif angle <= 200:
+            return (0, 1)
+        elif angle <= 245:
+            return (-1, 1)
+        elif angle <= 290:
+            return (-1, 0)
+        elif angle <= 335:
+            return (-1, -1)
+        else:
+            return (0, -1)
+        
+    def build(self):
+        angle = self.heroModel.getH()
+        pos = self.look_at(angle)
+        if self.isFreeMode:
+            self.land.addBlock(pos)
+        else:
+            self.land.buildBlock(pos)
+
+    def destroy(self):
+        pass
 
     def left(self):
         angle = (self.heroModel.getH() + 90) % 360
---
+++ minecraft/mapmanager.py
@@ -0,0 +1,60 @@
+from direct.showbase.ShowBase import ShowBase
+import pickle
+
+class MapManager():
+    def __init__(self):
+       pass
+
+    def startNew(self):
+        self.land = render.attachNewNode("Land")
+  
+    def getColor(self, z):
+       if z < len(self.colors):
+          return self.colors[z]
+       else:
+          return self.colors[len(self.colors) - 1]
+
+    
+    def addBlock(self, position):
+       self.block = loader.loadModel(self.model)
+       self.block.setTexture(loader.loadTexture(self.texture))
+       self.block.setPos(position)
+       self.color = self.getColor(int(position[2]))
+       self.block.setColor(color)
+       self.block.reparentTo(self.land)
+
+    def loadLand(self, filename):
+       pass
+
+    def clear(self):
+       pass
+
+    def findBlocks(self, pos):
+       pass
+
+
+    def isEmpty(self, pos):
+       pass
+
+
+    def findHighestEmpty(self, pos):
+       pass
+
+
+    def buildBlock(self, pos):
+       pass
+
+
+    def delBlock(self, position):
+       pass
+
+
+    def delBlockFrom(self, position):
+       pass
+
+    def saveMap(self):
+       pass
+
+
+    def loadMap(self):
+       pass
