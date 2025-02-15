c816def9f2522310bed4a3c986a12619132911e5
markgardie <60879509+markgardie@users.noreply.github.com>
2025-02-15 15:17:44 +0200

    minecraft, lesson 3

--- minecraft/mapmanager.py
+++ minecraft/mapmanager.py
@@ -2,59 +2,76 @@ from direct.showbase.ShowBase import ShowBase
 import pickle
 
 class MapManager():
-    def __init__(self):
-       pass
+   def __init__(self):
+      pass
 
-    def startNew(self):
-        self.land = render.attachNewNode("Land")
+   def startNew(self):
+      self.land = render.attachNewNode("Land")
   
-    def getColor(self, z):
-        if z < len(self.colors):
-           return self.colors[z]
-        else:
-           return self.colors[len(self.colors) - 1]
+   def getColor(self, z):
+      if z < len(self.colors):
+         return self.colors[z]
+      else:
+         return self.colors[len(self.colors) - 1]
 
     
-    def addBlock(self, position):
-        self.block = loader.loadModel(self.model)
-        self.block.setTexture(loader.loadTexture(self.texture))
-        self.block.setPos(position)
-        self.color = self.getColor(int(position[2]))
-        self.block.setColor(self.color)
-        self.block.reparentTo(self.land)
+   def addBlock(self, position):
+      self.block = loader.loadModel(self.model)
+      self.block.setTexture(loader.loadTexture(self.texture))
+      self.block.setPos(position)
+      self.color = self.getColor(int(position[2]))
+      self.block.setColor(self.color)
+      self.block.reparentTo(self.land)
 
-    def loadLand(self, filename):
+   def loadLand(self, filename):
+      self.clear()
+      with open(filename) as file:
+         y = 0
+         for row in file:
+            x = 0
+            row = row.split(" ")
+            for cell in row:
+               for height in range(int(cell) + 1):
+                  block = self.addBlock((x, y, height))
+               x += 1
+            y += 1
+
+      return x, y
+
+   def clear(self):
+      self.land.removeNode()
+      self.startNew()
+
+   def findBlocks(self, pos):
+      return self.land.findAllMatches("=at=" + str(pos))
+
+
+   def isEmpty(self, pos):
+       blocks = self.findBlocks(pos)
+       if blocks:
+           return False
+       else:
+           return True
+
+
+   def findHighestEmpty(self, pos):
        pass
 
-    def clear(self):
-       pass
 
-    def findBlocks(self, pos):
-       pass
-
-
-    def isEmpty(self, pos):
+   def buildBlock(self, pos):
        pass
 
 
-    def findHighestEmpty(self, pos):
+   def delBlock(self, position):
        pass
 
 
-    def buildBlock(self, pos):
+   def delBlockFrom(self, position):
+       pass
+
+   def saveMap(self):
        pass
 
 
-    def delBlock(self, position):
-       pass
-
-
-    def delBlockFrom(self, position):
-       pass
-
-    def saveMap(self):
-       pass
-
-
-    def loadMap(self):
+   def loadMap(self):
        pass
