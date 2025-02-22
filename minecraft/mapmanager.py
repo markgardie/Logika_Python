144273c7ed81f4492686f86a2a5491356aedf4ea
markgardie <60879509+markgardie@users.noreply.github.com>
2025-02-22 11:20:53 +0200

    minecraft, lesson 4

--- minecraft/mapmanager.py
+++ minecraft/mapmanager.py
@@ -2,59 +2,84 @@ from direct.showbase.ShowBase import ShowBase
 import pickle
 
 class MapManager():
-    def __init__(self):
-       pass
+   def __init__(self):
+      self.model = 'block.egg'
+      self.texture = 'block.png'
+      self.colors = [
+         (0.2, 0.2, 0.35, 1),
+         (0.2, 0.5, 0.2, 1),
+         (0.7, 0.2, 0.2, 1),
+         (0.5, 0.3, 0.0, 1)
+      ]
+      self.startNew()
 
-    def startNew(self):
-        self.land = render.attachNewNode("Land")
+   def startNew(self):
+      self.land = render.attachNewNode("Land")
   
-    def getColor(self, z):
-       if z < len(self.colors):
-          return self.colors[z]
-       else:
-          return self.colors[len(self.colors) - 1]
+   def getColor(self, z):
+      if z < len(self.colors):
+         return self.colors[z]
+      else:
+         return self.colors[len(self.colors) - 1]
 
     
-    def addBlock(self, position):
-       self.block = loader.loadModel(self.model)
-       self.block.setTexture(loader.loadTexture(self.texture))
-       self.block.setPos(position)
-       self.color = self.getColor(int(position[2]))
-       self.block.setColor(color)
-       self.block.reparentTo(self.land)
+   def addBlock(self, position):
+      self.block = loader.loadModel(self.model)
+      self.block.setTexture(loader.loadTexture(self.texture))
+      self.block.setPos(position)
+      self.color = self.getColor(int(position[2]))
+      self.block.setColor(color)
+      self.block.reparentTo(self.land)
 
-    def loadLand(self, filename):
-       pass
+   def loadLand(self, filename):
+      self.clear()
+      with open(filename) as file:
+         y = 0
+         for row in file:
+            x = 0
+            row = row.split(" ")
+            for cell in row:
+               for height in range(int(cell) + 1):
+                  block = self.addBlock((x, y, height))
+               x += 1
+            y+=1
 
-    def clear(self):
-       pass
+      return x, y
 
-    def findBlocks(self, pos):
-       pass
+   def clear(self):
+      self.land.removeNode()
+      self.startNew()
 
-
-    def isEmpty(self, pos):
-       pass
-
-
-    def findHighestEmpty(self, pos):
-       pass
-
-
-    def buildBlock(self, pos):
-       pass
-
-
-    def delBlock(self, position):
-       pass
+   def findBlocks(self, pos):
+      return self.land.findAllMatches("=at=" + str(pos))
 
 
-    def delBlockFrom(self, position):
-       pass
+   def isEmpty(self, pos):
+      blocks = self.findBlocks(pos)
+      if blocks:
+         return False
+      else:
+         return True
 
-    def saveMap(self):
-       pass
+
+   def findHighestEmpty(self, pos):
+      pass
 
 
-    def loadMap(self):
-       pass
+   def buildBlock(self, pos):
+      pass
+
+
+   def delBlock(self, position):
+      pass
+
+
+   def delBlockFrom(self, position):
+      pass
+
+   def saveMap(self):
+      pass
+
+
+   def loadMap(self):
+      pass
