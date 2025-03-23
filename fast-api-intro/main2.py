51527cfeebca6371008fd2eba94f1ac8478af61e
markgardie <60879509+markgardie@users.noreply.github.com>
2025-03-23 13:37:28 +0200

    fastapi, lesson 3

--- fast-api-intro/main2.py
+++ fast-api-intro/main2.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, HTTPException, Query
-from pydantic import BaseModel, Field, field_validator
-from typing import Dict, List, Optional
+from fastapi import FastAPI, HTTPException, Query, Path, Body, Depends
+from pydantic import BaseModel, Field
+from typing import Dict, List, Optional, Annotated
+from fastapi.responses import JSONResponse
 
 app = FastAPI(title="Book Catalog API")
 
@@ -12,24 +13,50 @@ class Book(BaseModel):
 library: Dict[str, Dict[str, Book]] = {}
 
 
+def validate_author(
+        author: Annotated[
+            str, 
+            Query(min_length=2, max_length=30, description="Ім'я повинно бути від 2 до 30")
+        ]
+    ):
+    return author
+
+def check_author_exists(author: str):
+    if author not in library:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Автор {author} не знайдений"
+        )
+
+    return author
+
 @app.post("/books/", status_code=201)
-async def create_book():
+async def create_book(
+    title: Annotated[str, Body(gt=2)],
+    author: Annotated[str, Body(min_length=2, max_length=30)],
+    pages: Annotated[int, Body(gt=10)]  
+):
     pass
 
 @app.get("/books/author/{author}")
-async def get_books_by_author():
+async def get_books_by_author(
+    author: Annotated[str, Path(min_length=2, max_length=30)] = Depends(check_author_exists)
+):
     pass
 
 @app.get("/books/")
-async def get_all_books():
+async def get_all_books( ):
     pass
 
-@app.put("/books/")
+@app.put("/books/{title}")
 async def update_book():
     pass
 
-@app.delete("/books/")
-async def delete_book():
+@app.delete("/books/{title}")
+async def delete_book(
+    title: Annotated[str, Path(gt=2)],
+    author: Annotated[str, Query(min_length=2, max_length=30)] = Depends(validate_author)
+):
     pass
 
 @app.get("/authors/")
