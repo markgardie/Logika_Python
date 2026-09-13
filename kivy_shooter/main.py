494f95e259dbccb743ba719b80909db33a155431
markgardie <60879509+markgardie@users.noreply.github.com>
2026-09-13 19:34:04 +0300

    kivy md, lesson 3

--- kivy_shooter/main.py
+++ kivy_shooter/main.py
@@ -4,40 +4,100 @@ from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
 from kivy.clock import Clock
 from kivy.metrics import sp, dp
-from kivy import platform
 from kivy.core.window import Window
+from kivy import platform
+from kivy.uix.image import Image
+from random import randint
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
+from kivy.core.window import Keyboard
+
 
 FPS = 60
 BULLET_SPEED = dp(10)
 SHIP_SPEED = dp(5)
 
+DIR_UP = 1
+DIR_DOWN = -1
+
 class Shot(MDWidget):
-    ...
+    def __init__(self, direction, **kwargs):
+        super().__init__(**kwargs)
+        self.direction = direction
 
 class MainScreen(MDScreen):
     ...
 
-class GameScreen(MDScreen):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        Clock.schedule_interval(self.update, 1/FPS)
-        self.eventkeys = {}
-        self.cartridge = []
+class Ship(Image):
+    def __init__(self, direction = DIR_UP, **kwargs):
+        super().__init__(**kwargs)
+        self.direction = direction
 
+    def moveLeft(self):
+        self.pos[0] -= SHIP_SPEED
+    
+    def moveRight(self):
+        self.pos[0] += SHIP_SPEED
 
-    def update(self, dt):
-        for key in self.eventkeys:
-            if self.eventkeys[key]:
-                if key == "left":
+    def shot(self):
+        shot = Shot(self.direction)
+        shot.center_x = self.center_x
+        shot.y = self.top if self.direction == DIR_UP else self.y - shot.height
+        self.parent.parent.parent.parent.bullets.append(shot)
+        self.parent.add_widget(shot)
+
+    def update(self):
+        pass
+
+class PlayerShip(Ship):
+    def __init__(self, direction=DIR_UP, **kwargs):
+        super().__init__(direction, **kwargs)
+
+    def update(self, keys):
+        for key in keys:
+            if keys[key]:
+                if key == "left" and self.center_x > 0:
                     self.moveLeft()
-                if key == "right":
+                if key == "right" and self.center_x < Window.width:
                     self.moveRight()
                 if key == "shot":
                     self.shot()
+                    keys[key] = False
+    
+class EnemyShip(Ship):
+    def __init__(self, direction=DIR_DOWN, **kwargs):
+        super().__init__(direction, **kwargs)
+        self.frame = 0
 
-        for bullet in self.cartridge:
-            bullet.pos[1] += BULLET_SPEED
+    def update(self):
+        super().update()
+        self.pos[1] -= dp(3)
+        if self.frame % 100 == 0:
+            self.shot()
+        self.frame += 1
 
+class GameScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.eventkeys = {}
+        self.enemyShips = []
+        self.bullets = []
+
+        self.ship = self.ids.ship
+
+        self.pauseMenu = None
+
+        # desktop
+        Window.bind(on_key_down = self._on_key_down)
+        Window.bind(on_key_up = self._on_key_up)
+
+    def update(self):
+        self.ship.update(self.eventkeys)
+
+        for enemy in self.enemyShips:
+            enemy.update()
+
+        self.manage_bullets()
 
     def pressKey(self, key):
         self.eventkeys[key] = True
@@ -45,17 +105,7 @@ class GameScreen(MDScreen):
     def releaseKey(self, key):
         self.eventkeys[key] = False
 
-    def moveLeft(self):
-        self.ids.ship.pos[0] -= SHIP_SPEED
 
-    def moveRight(self):
-        self.ids.ship.pos[0] += SHIP_SPEED
-
-    def shot(self):
-        shot = Shot(pos = (self.ids.ship.centerx,
-                           self.ids.ship.top))
-        self.cartridge.append(shot)
-        self.ids.front.add_widget(shot)
 
 class ShooterApp(MDApp):
     def build(self):
