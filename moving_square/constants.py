e577b636d2d28b04e096ec2e5d6942e9bb4eff95
markgardie <60879509+markgardie@users.noreply.github.com>
2026-09-20 15:59:06 +0300

    moving square, lesson 1

---
+++ moving_square/constants.py
@@ -0,0 +1,23 @@
+TITLE = "Moving Square"
+FPS = 60
+
+# Розміри вікна
+WIN_W, WIN_H = 800, 600
+
+# Кольори
+C_BG         = (15,  15,  25)
+C_PLAYER     = (80,  160, 255)
+C_WIN_RECT   = (80,  220, 120)
+C_LOSE_RECT  = (220, 70,  70)
+C_TEXT       = (230, 230, 230)
+C_SUBTEXT    = (150, 150, 160)
+C_BTN        = (40,  40,  60)
+C_BTN_HOVER  = (65,  65,  95)
+C_BTN_BORDER = (100, 100, 140)
+
+# Швидкість гравця (пікселів/кадр)
+PLAYER_SPEED = 4
+
+# Розміри об'єктів
+PLAYER_SIZE = 40
+TARGET_SIZE = 60
